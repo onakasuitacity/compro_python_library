07515270fc6251adb5791f572486eed7b7d17589
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-08-26 20:46:04 +0900

    Create 09_otoshidama.py

---
+++ ABS/09_otoshidama.py
@@ -0,0 +1,16 @@
+# https://atcoder.jp/contests/abs/tasks/abc085_c
+N,Y = map(int,input().split())
+Y = Y//1000
+flag = False
+
+for i in range(N+1):
+    for j in range(N+1-i):
+        if 10*i+5*j+(N-i-j) == Y:
+            print(i,j,N-i-j)
+            flag = True
+            break
+    if flag:
+        break
+
+if flag is False:
+    print(-1,-1,-1)
