42040605af05c6f5d15a5412cfa97571a684bf14
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-08-26 19:47:52 +0900

    Create 04_shift_only.py

---
+++ ABS/04_shift_only.py
@@ -0,0 +1,16 @@
+# https://atcoder.jp/contests/abs/tasks/abc081_b
+N = int(input())
+nums = list(map(int,input().split()))
+flag = True
+count = 0
+
+while(True):
+    for num in nums:
+        flag = flag and (num%2==0)
+    if flag:
+        count += 1
+        nums = list(map(lambda x:x//2, nums))
+    else:
+        break
+
+print(count)
