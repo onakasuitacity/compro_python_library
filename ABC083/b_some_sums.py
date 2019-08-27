4cc8972fbb5064081a79061d5a4797d4dec01136
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-08-27 18:17:00 +0900

    Create b_some_sums.py

---
+++ ABC083/b_some_sums.py
@@ -0,0 +1,10 @@
+# https://atcoder.jp/contests/abs/tasks/abc083_b
+N,A,B = map(int,input().split())
+count = 0
+
+for i in range(N+1):
+    s = sum(map(int,list(str(i))))
+    if A <= s and s <= B:
+        count += i
+
+print(count)
