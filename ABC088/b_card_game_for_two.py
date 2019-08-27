3a1099c6b3338d64a0a831edf3a700e067efe814
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-08-27 18:18:27 +0900

    Create b_card_game_for_two.py

---
+++ ABC088/b_card_game_for_two.py
@@ -0,0 +1,12 @@
+# https://atcoder.jp/contests/abs/tasks/abc088_b
+N = int(input())
+nums = sorted(map(int,input().split()),reverse=True)
+count = 0
+
+for i in range(N):
+    if i%2==0:
+        count += nums[i]
+    else:
+        count -= nums[i]
+        
+print(count)
