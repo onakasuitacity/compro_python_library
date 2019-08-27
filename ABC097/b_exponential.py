3d40bb0af9627c81d0cb21b6aa2ed6d319d0daea
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-08-28 07:31:27 +0900

    Create b_exponential.py

---
+++ ABC097/b_exponential.py
@@ -0,0 +1,10 @@
+# https://atcoder.jp/contests/abc097/tasks/abc097_b
+import itertools
+x = int(input())
+score = 0
+
+# bmax = int(math.sqrt(x)), pmax = len(bin(x))-3
+for b,p in itertools.product(range(1,32),range(2,9)):
+    if b**p<=x: score = max(score, b**p)
+
+print(score)
