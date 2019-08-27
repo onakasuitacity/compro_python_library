d2720853cb3fbcac5ab629a3fd78a16e6d22c7f0
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-08-27 10:29:46 +0900

    Create 09_daydream.py

---
+++ ABS/09_daydream.py
@@ -0,0 +1,14 @@
+# https://atcoder.jp/contests/abs/tasks
+import re
+S = input()[::-1]
+flag = True
+
+while(S):
+    m = re.match("maerd|remaerd|esare|resare", S)
+    if m:
+        S = S[m.end():]
+    else:
+        flag = False
+        break
+        
+print("YES") if flag else print("NO")
