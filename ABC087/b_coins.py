63e9ac27bff9903c5039d7ae1941445096f0eeec
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-08-27 18:15:32 +0900

    Create b_coins.py

---
+++ ABC087/b_coins.py
@@ -0,0 +1,14 @@
+# https://atcoder.jp/contests/abs/tasks/abc087_b
+import itertools
+
+A = int(input())
+B = int(input())
+C = int(input())
+X = int(input())//50
+count = 0
+
+for i,j,k in itertools.product(range(A+1),range(B+1),range(C+1)):
+    if i*10+j*2+k == X:
+        count += 1
+
+print(count)
