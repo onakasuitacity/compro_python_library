3d588d9709b72b992d789bb645d764493aadbbf2
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-08-28 16:45:19 +0900

    Create a_名刺交換.py

---
+++ ARC010/a_名刺交換.py
@@ -0,0 +1,11 @@
+# https://atcoder.jp/contests/arc010/tasks/arc010_1
+n,m,a,b = map(int,input().split())
+
+for i in range(m):
+    if n<=a: n+=b
+    n -= int(input())
+    if n<0:
+        print(i+1)
+        break
+else:
+    print("complete")
