3ac087bf3d30edac574771d1f49e863011a18e62
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-05 15:46:03 +0900

    Create b_thin.py

---
+++ ABC049/b_thin.py
@@ -0,0 +1,8 @@
+# https://atcoder.jp/contests/abc049/tasks/abc049_b
+def resolve():
+    h,w=map(int,input().split())
+    for _ in range(h):
+        a = input()
+        print(a)
+        print(a)
+resolve()
