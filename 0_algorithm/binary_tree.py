df9020ba87430854f3c8442aa0a7180c2e4c987b
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-09 16:41:54 +0900

    Create binary_tree.py

---
+++ 0_algorithm/binary_tree.py
@@ -0,0 +1,60 @@
+# https://engineeringnote.hateblo.jp/entry/python/algorithm-and-data-structures/binary_tree
+
+class Node(object):
+    def __init__(self,label):
+        self.label=str(label)
+        self.left=None
+        self.right=None
+
+    def __repr__(self):
+        return self.label
+
+### build tree
+#       A
+#      / \
+#     B   H
+#    / \
+#   C   D
+#      / \
+#     E   F
+#    /
+#   G
+root = Node('A')
+root.left = Node('B')
+root.right = Node('H')
+root.left.left = Node('C')
+root.left.right = Node('D')
+root.left.right.left = Node('E')
+root.left.right.right = Node('F')
+root.left.right.left.left = Node('G')
+
+### traverse
+# Preorder (A,B,C,D,E,G,F,H)
+def preorder(node):
+    if node==None: return
+    print(node,end=',')
+    preorder(node.left)
+    preorder(node.right)
+
+# Inorder (C,B,G,E,D,F,A,H)
+def inorder(node):
+    if node==None: return
+    inorder(node.left)
+    print(node,end=',')
+    inorder(node.right)
+
+# Postorder (C,G,E,F,D,B,H,A)
+def postorder(node):
+    if node==None: return
+    postorder(node.left)
+    postorder(node.right)
+    print(node,end=',')
+
+print("--- preorder ---")
+preorder(root)
+print()
+print("--- inorder ---")
+inorder(root)
+print()
+print("--- postorder ---")
+postorder(root)
