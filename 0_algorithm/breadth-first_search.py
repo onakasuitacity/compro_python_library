7d941019b96c3a284bb2788385ffc4d938ec0162
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-12 02:35:47 +0900

    Create breadth-first_search.py

---
+++ 0_algorithm/breadth-first_search.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+# given
+N=5
+G=list(range(N))
+E=[{1,2,3},{0},{0},{0,4},{3}]
+
+# parameter
+root=0
+
+# build
+inf=float("inf")
+d=[inf if i!=root else 0 for i in G]
+Q=deque()
+Q.append(root)
+while(Q):
+    u=Q.popleft()
+    for v in E[u]:
+        if d[v]>d[u]+1:
+            d[v]=d[u]+1
+            Q.append(v)
+
+# result
+print(d)
