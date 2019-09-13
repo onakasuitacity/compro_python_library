a2a13bad8bb5999618c4ff65621312cc88124d9a
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-14 02:09:51 +0900

    Update LCA_and_doubling.py

--- 0_algorithm/LCA_and_doubling.py
+++ 0_algorithm/LCA_and_doubling.py
@@ -1,50 +1,57 @@
 #%% LCA(Lowest Common Ancestor) & doubling
 # https://tjkendev.github.io/procon-library/python/graph/lca-doubling.html
 
-class LCA(object):
-    def __init__(self,edges,root): # edges : adjacence list
-        self.edges=edges
-        self.root=root
-        self.n=len(edges) # n=|V|
-        self.logn=(self.n-1).bit_length() # logn = ceil(log2(n))
-        # initialization
-        self.depth=[float("inf") if i!=root else 0 for i in range(self.n)]
-        self.parent=[[-1]*self.logn for _ in range(self.n)] # parent[u][i]=v : uの2^i世代上がv(なければ-1)
+class LCA:
+    """
+    construct: O(NlogN)
+    query: O(logN)
+    """
+
+    def __init__(self,edges,root=0):
+        """
+        :param list of (list of int) edges:
+        :param int root=0:
+        """
+        self.edges = edges
+        self.n = len(edges) # n=|V|
+        self.logn = (self.n-1).bit_length() # logn=ceil(log2(n))
+        self.depths = [-1] * self.n
+        self.parents = [[-1] * self.n for _ in range(self.logn)]
         # construct
-        self.dfs(root)
-        self.doubling()
+        self.__dfs(-1,root,0)
+        self.__doubling()
 
-    def dfs(self,u):
-        for v in self.edges[u]:
-            if self.depth[v]==float("inf"): # 訪れていない＝深さがINF(rootから連結な部分しか探索しない)
-                self.depth[v]=self.depth[u]+1
-                self.parent[v][0]=u
-                self.dfs(v)
+    def __dfs(self,par,cur,dep):
+        self.depths[cur]=dep
+        self.parents[0][cur]=par
+        for v in self.edges[cur]:
+            if self.depths[v]==-1: # 深さ-1＝訪れていない(rootから連結な部分しか探索しない)
+                self.__dfs(cur,v,dep+1)
 
-    def doubling(self):
+    def __doubling(self):
         for i in range(1,self.logn):
             for v in range(self.n):
-                if self.parent[v][i-1]!=-1: # 半分遡った時点で親が存在しなければ-1のままにしておく。-1が返るとlistの末尾の値の取得になってしまう
-                    self.parent[v][i]=self.parent[self.parent[v][i-1]][i-1]
-    
-    def get(self,u,v): # uとvとのLCAを返す
-        dd=self.depth[v]-self.depth[u]
+                if self.parents[i-1][v]==-1: # 親が存在しないとき。-1を通すとlistの末尾を取得してしまう
+                    continue
+                self.parents[i][v]=self.parents[i-1][self.parents[i-1][v]]
+
+    def get(self,u,v): # u,vのLCAを返す
+        dd=self.depths[v]-self.depths[u]
         if dd<0: # vの方が深いようにする
             u,v=v,u
-            dd=-dd
+            dd*=-1
+        for i in range(self.logn):
+            if dd&(1<<i):
+                v = self.parents[i][v]
 
-        for i in range(self.logn): # dd分だけvを遡らせる
-            if dd&1: # 各bitで判定
-                v=self.parent[v][i]
-            dd>>=1
+        if v==u: return v # 高さ揃えた時点で一致してたら終わり
 
-        if u==v: return u # 高さ揃えた時点で一致してたら終わり
-
-        for i in range(self.logn-1,-1,-1): # そうでなければ上から二分探索
-            pu,pv=self.parent[u][i],self.parent[v][i]
+        # そうでなければ上から二分探索
+        for i in reversed(range(self.logn)):
+            pu,pv=self.parents[i][u],self.parents[i][v]
             if pu!=pv:
                 u,v=pu,pv
-        return self.parent[u][0]
+        return self.parents[0][u]
                 
 #%%
 # given data
