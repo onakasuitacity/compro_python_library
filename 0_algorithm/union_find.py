587ce16f6491e412eb55a76ba7ef8c5c47393190
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-17 12:27:29 +0900

    Update union_find.py

--- 0_algorithm/union_find.py
+++ 0_algorithm/union_find.py
@@ -2,28 +2,35 @@
 # https://www.slideshare.net/chokudai/union-find-49066733
 class UnionFind(object):
     """
-    query: O(logN) (amortize)
+    query: O(Ack^-1(n,n)) (amortize)
     """
     def __init__(self,n):
         """
         param n: number of nodes
         """
         self.__par=list(range(n))
+        self.__rank=[0]*n
 
-    def __get_root(self,k):
+    def __root(self,k):
         if self.__par[k]==k: return k
         else:
-            self.__par[k]=self.__get_root(self.__par[k])
+            self.__par[k]=self.__root(self.__par[k])
             return self.__par[k]
 
     def same(self,i,j):
-        return self.__get_root(i)==self.__get_root(j)
+        return self.__root(i)==self.__root(j)
 
     def unite(self,i,j):
-        i=self.__get_root(i)
-        j=self.__get_root(j)
+        i=self.__root(i)
+        j=self.__root(j)
         if i==j: return
-        self.__par[i]=j
+        if self.__rank[i]>self.__rank[j]: # rankが大きいほうが親になる
+            self.__par[j]=i
+        elif self.__rank[i]<self.__rank[j]:
+            self.__par[j]=i
+        else: # rankが同じときはどっちでもよい
+            self.__par[j]=i
+            self.__rank[i]+=1
 
 #%%
 union=UnionFind(6)
