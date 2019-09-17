1e1a0bdabbcffdcdb8d46e1c6125137eb88ce42d
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-17 23:45:33 +0900

    Update functions.py

--- 1_functions/functions.py
+++ 1_functions/functions.py
@@ -46,3 +46,11 @@ def modinv(a,m):
     u%=m
     if u<0: u+=m
     return u
+
+# power set
+# https://qiita.com/hrsma2i/items/d98f9b1fceeb23d67eef
+from itertools import chain,combinations
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
