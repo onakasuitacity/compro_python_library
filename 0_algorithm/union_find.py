9277ee86ff95690e6f53cb5a4ccb3bbb2cf0b0b2
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-17 11:10:18 +0900

    Create union_find.py

---
+++ 0_algorithm/union_find.py
@@ -0,0 +1,35 @@
+# Union Find (with Path compression)
+# https://www.slideshare.net/chokudai/union-find-49066733
+class UnionFind(object):
+    """
+    construct: O(N)
+    query: O(logN)
+    """
+    def __init__(self,n):
+        """
+        param n: number of nodes
+        """
+        self.__par=list(range(n))
+
+    def __get_root(self,k):
+        if self.__par[k]==k: return k
+        else:
+            self.__par[k]=self.__get_root(self.__par[k])
+            return self.__par[k]
+
+    def same(self,i,j):
+        return self.__get_root(i)==self.__get_root(j)
+
+    def unite(self,i,j):
+        i=self.__get_root(i)
+        j=self.__get_root(j)
+        if i==j: return
+        self.__par[i]=j
+
+#%%
+union=UnionFind(6)
+union.unite(0,2)
+union.unite(1,3)
+union.unite(1,5)
+print(union.same(3,5))
+print(union.same(2,4))
