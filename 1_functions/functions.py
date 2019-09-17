502398d187d6cba19aab3623722eef406666f7ba
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-17 09:52:41 +0900

    Update functions.py

--- 1_functions/functions.py
+++ 1_functions/functions.py
@@ -34,3 +34,15 @@ def is_prime(n):
     for a in A:
         P.append(a)
     return n in P
+
+# aのZ/mZでのinverse
+# https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
+def modinv(a,m):
+    b,u,v=m,1,0
+    while(b):
+        t=a//b
+        a-=t*b; a,b=b,a
+        u-=t*v; u,v=v,u
+    u%=m
+    if u<0: u+=m
+    return u
