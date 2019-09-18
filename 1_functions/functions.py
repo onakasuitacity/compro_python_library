89a45d85ace6f20d7b42a69230e714b7ad14f4f9
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-19 03:52:47 +0900

    Update functions.py

--- 1_functions/functions.py
+++ 1_functions/functions.py
@@ -73,3 +73,16 @@ def power(b,p,f):
             if res=='$': res=S[i]
             else: res=f(res,S[i])
     return res
+
+# matrix multiple (ijk-algorithm)
+def mat_mul(A,B):
+    assert len(A[0])==len(B)
+    m=len(A)
+    n=len(B)
+    l=len(B[0])
+    C=[[0]*l for _ in range(m)]
+    for i in range(m):
+        for j in range(l):
+            for k in range(n):
+                C[i][j]+=A[i][k]*B[k][j]
+    return C
