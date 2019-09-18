d6f7b64ebf5262738c3309db58ac175b5a9526e6
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-19 03:15:31 +0900

    Update functions.py

--- 1_functions/functions.py
+++ 1_functions/functions.py
@@ -54,3 +54,25 @@ def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+# Exponential (O(logp))
+def power(b,p,f):
+    """
+    b: base
+    p: positive int (multiplier)
+    f: multiple function
+    return: f^p(b)
+    """
+    if not isinstance(p,int): raise ValueError("multiplier must be int")
+    elif p<=0: raise ValueError("multiplier must be positive.")
+    elif p==1: return b
+    logp=(p-1).bit_length()
+    S=[0]*logp
+    S[0]=b
+    res='$'
+    for i in range(logp):
+        if i!=logp-1: S[i+1]=f(S[i],S[i])
+        if p&(1<<i):
+            if res=='$': res=S[i]
+            else: res=f(res,S[i])
+    return res
