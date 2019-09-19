b5e6fc0abe1d9922457353805ebfdb3d230fc3da
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-20 03:41:38 +0900

    Update binary_search.py

--- 0_algorithm/binary_search.py
+++ 0_algorithm/binary_search.py
@@ -1,20 +1,22 @@
-# binary search (O(log(r-l)))
-def bisect(l,r,f,left=True):
+# bisection method (O(r-l))
+def bisect(l,r,f,discrete=True,left=True):
     """
     l,r: int (l<r)
-    f: {l,...,r} to {False,True}
+    f: function defined on [l,...,r] to {False,True}
+    if discrete: f defined on Z; else: R
     if left: f satisfies that there uniquely exists d such that iff i<=d then f(i)
     else: iff i>=d then f(i) is True
     return d such as those above
     """
     assert r>l
+    eps=1 if discrete else 10**-10
     if (not left)^f(r): return r if left else r+1
     elif left^f(l): return l-1 if left else l
-    while(True):
-        if r-l<=1: return l if left else r
-        h=(l+r)//2
+    while(r-l>eps):
+        h=(l+r)//2 if discrete else (l+r)/2
         if (not left)^f(h): l=h
         else: r=h
+    return l if left else r
 
 #%%
-print(bisect(-4,10,lambda x:x>5.7,left=False))
+print(bisect(-4,10,lambda x:x>6,discrete=False,left=False))
