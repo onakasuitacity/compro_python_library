450adf96c1c6df5f4e031ba7c6cb82a7fd9cfc46
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-19 15:54:46 +0900

    Update d_射撃王.py

--- ABC023/d_射撃王.py
+++ ABC023/d_射撃王.py
@@ -30,7 +30,7 @@ def bisect(l,r,f,left=True):
     if (not left)^f(r): return r if left else r+1
     elif left^f(l): return l-1 if left else l
     while(True):
-        if r-l<=1: return l if f(l) else r
+        if r-l<=1: return l if left else r
         h=(l+r)//2
         if (not left)^f(h): l=h
         else: r=h
