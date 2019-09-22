0070a447663f0d0cd202e9cf31bad5f80d25b95d
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-22 09:26:10 +0900

    Update sparse_table.py

--- 0_algorithm/sparse_table.py
+++ 0_algorithm/sparse_table.py
@@ -1,77 +1,74 @@
 #%% Sparse Table O(NlogN)
 # http://tookunn.hatenablog.com/entry/2016/07/13/211148
-
 class SparseTable(object):
     """
     construct: O(NlogN)
-    query: O(1)
+    query:
+        max: O(1)
+        min: O(1)
     """
     def __init__(self,A):
         """
         A: list of float
         """
         # initialize
-        self.__A=A[:]
-        self.__n=len(A)
-        self.__logn=max(0,(self.__n-1).bit_length())
-        self.__maxtable=[[0]*self.__n for _ in range(self.__logn)]
-        self.__maxtable[0]=list(range(self.__n))
-        self.__mintable=[[0]*self.__n for _ in range(self.__logn)]
-        self.__mintable[0]=list(range(self.__n))
-        # construct(DP)
-        for i in range(1,self.__logn):
-            for k in range(self.__n):
-                # [k:k+1<<(i-1)]と[k+(1<<(i-1)):k+(1<<i)]とに分けることで、i-1の結果を使える
-                # k+(1<<(i-1))がindexを超えてるならば、前回の値がそのまま使える
-                if k+(1<<(i-1))>=self.__n:
-                    self.__maxtable[i][k]=self.__maxtable[i-1][k]
-                    self.__mintable[i][k]=self.__mintable[i-1][k]
-                    continue
-                # そうでないときは、2つの区間に分けたときのAの大きさで判断
-                # max
-                if self.__A[self.__maxtable[i-1][k]]>=self.__A[self.__maxtable[i-1][k+(1<<(i-1))]]:
-                    self.__maxtable[i][k]=self.__maxtable[i-1][k]
-                else:
-                    self.__maxtable[i][k]=self.__maxtable[i-1][k+(1<<(i-1))]
-                # min
-                if self.__A[self.__mintable[i-1][k]]<=self.__A[self.__mintable[i-1][k+(1<<(i-1))]]:
-                    self.__mintable[i][k]=self.__mintable[i-1][k]
-                else:
-                    self.__mintable[i][k]=self.__mintable[i-1][k+(1<<(i-1))]
-        
+        n=len(A)
+        logn=max(0,(n-1).bit_length())
+        maxtable=[[0]*n for _ in range(logn)]
+        maxtable[0]=A[:]
+        mintable=[[0]*n for _ in range(logn)]
+        mintable[0]=A[:]
+        # construct
+        from itertools import product
+        for i,k in product(range(1,logn),range(n)):
+            # [k:k+1<<(i-1)]と[k+(1<<(i-1)):k+(1<<i)]とに分けることで、i-1の結果を使える
+            # k+(1<<(i-1))がindexを超えてるならば、前回の値がそのまま使える
+            if k+(1<<(i-1))>=n:
+                maxtable[i][k]=maxtable[i-1][k]
+                mintable[i][k]=mintable[i-1][k]
+                continue
+            # そうでないときは、2つの区間に分けたときの大きさで判断
+            # max
+            if maxtable[i-1][k]>=maxtable[i-1][k+(1<<(i-1))]:
+                maxtable[i][k]=maxtable[i-1][k]
+            else:
+                maxtable[i][k]=maxtable[i-1][k+(1<<(i-1))]
+            # min
+            if mintable[i-1][k]<=mintable[i-1][k+(1<<(i-1))]:
+                mintable[i][k]=mintable[i-1][k]
+            else:
+                mintable[i][k]=mintable[i-1][k+(1<<(i-1))]
+        self.__n=n
+        self.__maxtable=maxtable
+        self.__mintable=mintable
+
     def max(self,l,r):
         """
-        param 0<=l<r<=n-1
-        return i such that A[i]=max(A[l:r])
+        l,r: int 0<=l<r<=n
+        return max(A[l:r])
         """
-        if r-l<=0: raise ValueError("l<r")
-        if r-l==1: return l
-        i=(r-l-1).bit_length()-1 # r-l>=2
-        if self.__A[self.__maxtable[i][l]]>=self.__A[self.__maxtable[i][r-(1<<i)]]:
-            return self.__maxtable[i][l]
-        else:
-            return self.__maxtable[i][r-(1<<i)]
+        n=self.__n
+        assert 0<=l<r<=n
+        i=max(0,(r-l-1).bit_length()-1)
+        maxtable=self.__maxtable
+        return max(maxtable[i][l],maxtable[i][r-(1<<i)])
 
     def min(self,l,r):
         """
-        param 0<=l<r<=n-1
-        return i such that A[i]=min(A[l:r])
+        l,r: int 0<=l<r<=n
+        return min(A[l:r])
         """
-        if r-l<=0: raise ValueError("l<r")
-        if r-l==1: return l
-        i=(r-l-1).bit_length()-1 # r-l>=2
-        if self.__A[self.__mintable[i][l]]<=self.__A[self.__mintable[i][r-(1<<i)]]:
-            return self.__mintable[i][l]
-        else:
-            return self.__mintable[i][r-(1<<i)]
-        
-#%% input
+        n=self.__n
+        assert 0<=l<r<=n
+        i=max(0,(r-l-1).bit_length()-1)
+        mintable=self.__mintable
+        return min(mintable[i][l],mintable[i][r-(1<<i)])
+
+#%% example
 A=[2,5,3,9,8,2,10,1,7,2,1,6]
 table=SparseTable(A)
 n=len(A)
 from itertools import product
-for l,r in product(range(n),repeat=2):
-    if r-l<=0: continue 
+for l,r in product(range(n),range(1,n+1)):
+    if r-l<=0: continue
     print(l,r,table.max(l,r),table.min(l,r))
-print(table.max(l,r))
-print(table.min(l,r))
