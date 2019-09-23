9da6d1a65dfd2058783b27980bf6bce2a9789521
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-23 13:03:05 +0900

    Update z-algorithm.py

--- 0_algorithm/z-algorithm.py
+++ 0_algorithm/z-algorithm.py
@@ -12,19 +12,16 @@ def Z(s):
     # construct
     L,R=0,0
     for i in range(1,n):
-        if i>R: # 過去の結果が全く使えない
+        if i>=R: # 過去の結果が全く使えない
             L=R=i
             while(R<n and s[R-L]==s[R]): R+=1
             z[i]=R-L
-            R-=1 # 0-indexedでiと比較するため
-        elif z[i-L]<R-i+1:
+        elif z[i-L]<R-i: # 過去の結果が全て使える
             z[i]=z[i-L]
-        else:
+        else: # 過去の結果が一部使える
             L=i
-            R+=1 # Rまでは確定しているので、R+1から確認
             while(R<n and s[R-L]==s[R]): R+=1
             z[i]=R-L
-            R-=1
     return z
 
 # input
