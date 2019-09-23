b570be80c9cb03514cf89ad06499c3f0fbec6f47
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-23 17:50:23 +0900

    Update functions.py

--- 1_functions/functions.py
+++ 1_functions/functions.py
@@ -32,6 +32,16 @@ def trial_division(n):
     if n==1: return factor
     else: return factor+[n]
 
+# divisors
+def divisors(n):
+    S,T=[],[]
+    for i in range(1,int(n**.5)+1):
+        if n%i==0:
+            S.append(i)
+            T.append(n//i)
+    T.reverse()
+    return S+T if S[-1]!=T[0] else S+T[1:]
+
 # aのZ/mZでのinverse
 # https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a
 def modinv(a,m):
