abea0044fdd8d079415ea9e1de5511e8cdc554f5
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-09-24 14:44:45 +0900

    Create a_addition_and_subtraction_easy.py

---
+++ ABC050/a_addition_and_subtraction_easy.py
@@ -0,0 +1,9 @@
+# https://atcoder.jp/contests/abc050/tasks/abc050_a
+import sys
+sys.setrecursionlimit(2147483647)
+INF=float("inf")
+MOD=10**9+7
+input=lambda :sys.stdin.readline().rstrip()
+def resolve():
+    print(eval(input()))
+resolve()
