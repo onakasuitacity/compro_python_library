ed48b2014b0f06815dfe5f0094b8cb0ee1f2b310
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-07 22:33:41 +0900

    Update lazy_propagation_segment_tree.py

--- 0_algorithm/lazy_propagation_segment_tree.py
+++ 0_algorithm/lazy_propagation_segment_tree.py
@@ -30,7 +30,7 @@ class SegmentTree(object):
         Left,Right=[],[]
         n=self.__n
         l+=n-1; r+=n-2
-        while(r-l>0):
+        while(l<r):
             if l%2==0:
                 Left.append(l)
             if r%2==1:
@@ -40,39 +40,63 @@ class SegmentTree(object):
         if l==r:
             if l%2==0: Left.append(l)
             else: Right.append(l)
-        Left.reverse(); Right.reverse()
         return Left,Right
 
-    def __pointwise_update(self,i):
+    def __propagate(self,i):
+        if self.__lazy[i]==self.__id: return
         node=self.__node
         lazy=self.__lazy
-        if lazy[i]!=self.__id:
-            if i<=self.__n-2: # propagate to children
-                lazy[2*i+1]=self.__comp(lazy[2*i+1],lazy[i])
-                lazy[2*i+2]=self.__comp(lazy[2*i+2],lazy[i])
-            node[i]=self.__act(lazy[i],node[i]) # action
-            lazy[i]=self.__id
-
-    def __range_update(self,l,r):
-        Left,Right=self.__get_range(l,r)
-        for i in Left+Right: self.__pointwise_update(i)
-        if l!=0: self.__pointwise_update(Left[-1]-1)
-        if r!=self.__n: self.__pointwise_update(Right[-1]+1)
-        # 下から上がっていくところを書く
+        if i<=self.__n-2: # propagate to children
+            lazy[2*i+1]=self.__comp(lazy[2*i+1],lazy[i])
+            lazy[2*i+2]=self.__comp(lazy[2*i+2],lazy[i])
+        node[i]=self.__act(lazy[i],node[i]) # action
+        lazy[i]=self.__id
 
     def add(self,l,r,f):
         Left,Right=self.__get_range(l,r)
         lazy=self.__lazy
         for i in Left+Right:
             lazy[i]=self.__comp(lazy[i],f)
-        self.__range_update(l,r)
-
+            self.__propagate(i)
+        if l!=0 and Left: self.__propagate(Left[0]-1)
+        if r!=self.__n and Right: self.__propagate(Right[0]+1)
+        node=self.__node
+        lowest=[]
+        if Left: lowest.append(Left[0])
+        if Right: lowest.append(Right[0])
+        for i in lowest:
+            while(i!=-1):
+                i=(i-1)//2
+                node[i]=self.__dot(node[2*i+1],node[2*i+2])
 
     def sum(self,l,r):
-        pass
+        Left,Right=self.__get_range(l,r)
+        for i in Left+Right: self.__propagate(i)
+        res=self.__e
+        for i in Left+Right[::-1]:
+            res=self.__dot(res,self.__node[i])
+        return res
 
-    def search(self,l,r,x):
-        pass
+    def bisect(self,l,r,x,increase=True,i=0,a=0,b=None):
+        """
+        if increase: return d such that S[i]<x iff i<=d (l<=i<l)
+        else: S[i]>x iff i<=d
+        where S is cummulative sum of A
+        """
+        self.__propagate(i)
+        if b is None: b=self.__n
+        if increase:
+            if self.__node[i]<=x or b<=l or r<=a: return -1
+            if i>=self.__n-1: return i-(self.__n-1)
+            lv=self.bisect(l,r,x,increase,2*i+1,a,(a+b)//2)
+            if lv!=-1: return lv
+            return self.bisect(l,r,x,increase,2*i+2,(a+b)//2,b)
+        else:
+            if self.__node[i]>=x or b<=l or r<=a: return -1
+            if i>=self.__n-1: return i-(self.__n-1)
+            rv=self.bisect(l,r,x,increase,2*i+2,(a+b)//2,b)
+            if rv!=-1: return rv
+            return self.bisect(l,r,x,increase,2*i+1,a,(a+b)//2)
 
 
 # RmQ and range add
@@ -80,4 +104,7 @@ INF=float("inf")
 A=[4,9,11,5,13,33,33,33,11,45,14,19,19,8,10,89]
 from operator import add
 tree=SegmentTree(A,min,INF,add,0,add)
-print(tree._SegmentTree__get_range(5,15))
+print(tree._SegmentTree__get_range(4,12))
+tree.add(2,8,4)
+print(tree.sum(1,5))
+print(tree.bisect(3,13,9.5,increase=False))
