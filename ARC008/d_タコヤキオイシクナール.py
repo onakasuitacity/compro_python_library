2b88e46c1675e5a088fceaa0a44281972f04a434
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-10 11:24:10 +0900

    Update d_タコヤキオイシクナール.py

--- ARC008/d_タコヤキオイシクナール.py
+++ ARC008/d_タコヤキオイシクナール.py
@@ -5,43 +5,53 @@ class SegmentTree(object):
         self.__n=n
         self.__dot=dot
         self.__e=e
-        self.__node=[e]*(2*n-1)
+        self.__node=[e]*(2*n)
         for i in range(len(A)):
-            self.__node[n+i-1]=A[i]
-        for i in range(n-2,-1,-1):
-            self.__node[i]=dot(self.__node[2*i+1],self.__node[2*i+2])
+            self.__node[i+n]=A[i]
+        for i in range(n-1,-0,-1):
+            self.__node[i]=dot(self.__node[2*i],self.__node[2*i+1])
 
     def update(self,i,c):
-        i+=self.__n-1
+        i+=self.__n
         node=self.__node
         node[i]=c
-        while(i!=0):
-            i=(i-1)//2
-            node[i]=self.__dot(node[2*i+1],node[2*i+2])
-
-    def __get_range(self,l,r):
-        if l>=r: return [],[]
-        Left,Right=[],[]
-        n=self.__n
-        l+=n-1; r+=n-2
-        while(l<r):
-            if l%2==0:
-                Left.append(l)
-            if r%2==1:
-                Right.append(r)
-                r-=1
-            l=l//2; r=(r-1)//2
-        if l==r:
-            if l%2==0: Left.append(l)
-            else: Right.append(l)
-        return Left,Right
+        while(i!=1):
+            i//=2
+            node[i]=self.__dot(node[2*i],node[2*i+1])
 
     def sum(self,l,r):
-        Left,Right=self.__get_range(l,r)
-        res=self.__e
-        for i in Left+Right[::-1]:
-            res=self.__dot(res,self.__node[i])
-        return res
+        vl,vr=self.__e,self.__e
+        l+=self.__n; r+=self.__n
+        while(l<r):
+            if(l%2==1):
+                vl=self.__dot(vl,self.__node[l])
+                l+=1
+            l//=2
+            if(r%2==1):
+                r-=1
+                vr=self.__dot(self.__node[r],vr)
+            r//=2
+        return self.__dot(vl,vr)
+
+    def bisect(self,l,r,x,increase=True,i=1,a=0,b=-1):
+        """
+        if increase: return d such that S[i]<x iff i<=d (l<=i<l)
+        else: S[i]>x iff i<=d
+        where S is cummulative sum of A
+        """
+        if b==-1: b=self.__n
+        if increase:
+            if self.__node[i]<=x or b<=l or r<=a: return -1
+            if i>=self.__n: return i-self.__n
+            lv=self.bisect(l,r,x,increase,2*i,a,(a+b)//2)
+            if lv!=-1: return lv
+            return self.bisect(l,r,x,increase,2*i+1,(a+b)//2,b)
+        else:
+            if self.__node[i]>=x or b<=l or r<=a: return -1
+            if i>=self.__n: return i-self.__n
+            rv=self.bisect(l,r,x,increase,2*i+1,(a+b)//2,b)
+            if rv!=-1: return rv
+            return self.bisect(l,r,x,increase,2*i,a,(a+b)//2)
 
 import sys
 sys.setrecursionlimit(2147483647)
@@ -60,11 +70,12 @@ def resolve():
         a,b=float(a),float(b)
         Point.add(p)
         Query.append((p,a,b))
+    # coordinate compression
     Point=sorted(Point)
     n=len(Point)
     id={Point[i]:i for i in range(n)}
     # segment tree
-    A=[e]*n # coordinate compression
+    A=[e]*n
     tree=SegmentTree(A,dot,e)
     res=[1]
     for p,a,b in Query:
