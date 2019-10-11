5e96f0122179052f5eb2d3100208ee44f09912f6
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-11 15:28:39 +0900

    Create Zobrist_hash.py

---
+++ 0_algorithm/Zobrist_hash.py
@@ -0,0 +1,18 @@
+class ZobristHash(object):
+    __mod1=10**9+7; __mod2=10**9+9
+
+    def __init__(self,s):
+        n=len(s)
+        self.__s=s
+        self.__n=len(s)
+        m1=self.__mod1; m2=self.__mod2
+        H1,H2=[1]*(n+1),[1]*(n+1)
+        for i in range(n):
+            H1[i+1]=ord(s[i])*H1[i]%m1
+            H2[i+1]=ord(s[i])*H2[i]%m2
+        self.__H1=H1; self.__H2=H2
+
+    def hash(self,l,r):
+        m1=self.__mod1; m2=self.__mod2
+        H1=self.__H1; H2=self.__H2
+        return (H1[r]*pow(H1[l],m1-2,m1)%m1,H2[r]*pow(H2[l],m2-2,m2)%m2)
