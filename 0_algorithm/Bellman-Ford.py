04ad90def8c529cc43947d9ad14d8657985e8682
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-12 14:10:34 +0900

    Update Bellman-Ford.py

--- 0_algorithm/Bellman-Ford.py
+++ 0_algorithm/Bellman-Ford.py
@@ -1,40 +1,34 @@
-#%% Bellman-Ford algorithm O(VE)
+# Bellman-Ford algorithm (O(MN))
 # http://wakabame.hatenablog.com/entry/2017/09/06/221400
-
 class BellmanFord(object):
-    """
-    construct: O(VE)
-    """
-
-    def __init__(self,edges,start=0):
+    def __init__(self,E,start=0):
         """
-        :param list of list of [node,cost] edges:
-        :param int start=0:
+        E: adjacency list with weight
         """
-        self.__n=len(edges)
+        self.__n=len(E)
         self.__dist=[float("inf")]*self.__n
         self.__dist[start]=0
-        self.__calculate(edges,start)
+        self.__calculate(E,start)
 
     @property
     def dist(self):
         return self.__dist
 
-    def __calculate(self,edges,start): # relaxingという
+    def __calculate(self,E,start): # relaxingという
+        dist=self.__dist
         for k in range(self.__n): # n回更新する。n回目に更新が行われたら負経路あり。
             update=False
             for v in range(self.__n):
-                for u,cost in edges[v]:
-                    if self.dist[u]>self.dist[v]+cost:
-                        self.__dist[u]=self.dist[v]+cost
+                for u,w in E[v]:
+                    if dist[u]>dist[v]+w:
+                        dist[u]=dist[v]+w
                         update=True
-            else:
-                if (update is False): # 更新されない＝終わり
-                    return
-                elif (update is True) and (k==self.__n-1): # 負の閉路検出
-                    self.__dist=[-float("inf")]*self.__n
+            if (update is False): # 更新されない＝終わり
+                return
+            elif (update is True) and (k==self.__n-1): # 負の閉路検出
+                self.__dist=[-float("inf")]*self.__n
 
-# input
+# example
 V=list(range(5))
 E=[
 [[1,50],[2,80]],
