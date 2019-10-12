4fc9d2e57d152a69ae3aae0ddd7f4aaedd803489
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-12 15:22:32 +0900

    Update rolling_hash.py

--- 0_algorithm/rolling_hash.py
+++ 0_algorithm/rolling_hash.py
@@ -13,9 +13,6 @@ class RollingHash(object):
     __base2=1009; __mod2=10**9+7
 
     def __init__(self,s):
-        """
-        s: str
-        """
         n=len(s)
         self.__s=s
         self.__n=n
@@ -31,58 +28,37 @@ class RollingHash(object):
         self.__H1=H1; self.__H2=H2
         self.__P1=P1; self.__P2=P2
 
-    @property
-    def str(self):
-        return self.__s
-    
-    @property
-    def len(self):
-        return self.__n
-
     def hash(self,l,r=None):
-        """
-        l,r: int (0<=l<=r<=n)
-        return (hash1,hash2) of S[l:r]
-        """
         m1=self.__mod1; m2=self.__mod2
-        if r is None: r=self.__n
+        if(r is None): r=self.__n
         assert 0<=l<=r<=self.__n
         return ((self.__H1[r]-self.__P1[r-l]*self.__H1[l]%m1)%m1,
                 (self.__H2[r]-self.__P2[r-l]*self.__H2[l]%m2)%m2)
 
     @classmethod
     def lcp(cls,rh1,rh2,l1,l2,r1=None,r2=None):
-        """
-        rh1,rh2: RollingHash object
-        l1,l2,r1,r2: int 0<=l1<=r1<=r1.len,0<=l2<=r2<=rh2.len
-        return lcp length between rh1[l1:r1] and rh2[l2:r2]
-        """
-        if r1 is None: r1=rh1.__n
-        if r2 is None: r2=rh2.__n
+        if(r1 is None): r1=rh1.__n
+        if(r2 is None): r2=rh2.__n
         assert 0<=l1<=r1<=rh1.__n and 0<=l2<=r2<=rh2.__n
         L=0
         R=min(r1-l1,r2-l2)
-        if rh1.hash(l1,l1+R)==rh2.hash(l2,l2+R): return R
+        if(rh1.hash(l1,l1+R)==rh2.hash(l2,l2+R)): return R
         while(R-L>1):
             H=(L+R)//2
-            if rh1.hash(l1,l1+H)==rh2.hash(l2,l2+H): L=H
+            if(rh1.hash(l1,l1+H)==rh2.hash(l2,l2+H)): L=H
             else: R=H
         return L
 
     @classmethod
     def search(cls,pattern,text):
-        """
-        pattern,text: RollingHash object
-        return list of index i's satisfying text[i:] starts with pattern
-        """
         n=text.__n; m=pattern.__n
         res=[]
         for i in range(n-m+1):
-            if text.hash(i,i+m)==pattern.hash(0,m):
+            if(text.hash(i,i+m)==pattern.hash(0,m)):
                 res.append(i)
         return res
 
-#%% example
+# example
 s="unvhusmjlvieloveuybouqvnqjygutqlovedkfsdfgheaiuloveaeiuvaygayfg"
 t="love"
 s=RollingHash(s)
