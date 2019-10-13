2e611d10967810f564b2b766d896c5023fe14c8a
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-14 03:02:10 +0900

    Update union_find.py

--- 0_algorithm/union_find.py
+++ 0_algorithm/union_find.py
@@ -1,28 +1,29 @@
 # Union Find (amortized O(ack^-1(N)))
 # https://www.slideshare.net/chokudai/union-find-49066733
 class UnionFind(object):
-    def __init__(self,n):
+    from operator import add
+    def __init__(self,n,size=None,add=add):
         self.__par=list(range(n))
         self.__rank=[0]*n
-        self.__size=[1]*n
+        self.__size=size if size else [1]*n
+        self.__add=add
 
     def root(self,k):
         if(self.__par[k]==k): return k
-        else:
-            self.__par[k]=self.root(self.__par[k])
-            return self.__par[k]
+        self.__par[k]=self.root(self.__par[k])
+        return self.__par[k]
 
     def unite(self,i,j):
-        i=self.root(i)
-        j=self.root(j)
+        i=self.root(i); j=self.root(j)
+        par=self.__par; rank=self.__rank; size=self.__size
         if(i==j): return
-        if(self.__rank[i]>self.__rank[j]):
-            self.__par[j]=i
-            self.__size[i]+=self.__size[j]
+        if(rank[i]>rank[j]):
+            par[j]=i
+            size[i]=self.__add(size[i],size[j])
         else:
-            self.__par[i]=j
-            self.__size[j]+=self.__size[i]
-            if(self.__rank[i]==self.__rank[j]): self.__rank[j]+=1
+            par[i]=j
+            size[j]=self.__add(size[i],size[j])
+            if(rank[i]==rank[j]): rank[j]+=1
 
     def size(self,k):
         return self.__size[self.root(k)]
