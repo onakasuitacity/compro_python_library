0c6674061c07986784ff2ff764d2c14259895cc3
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-18 18:57:49 +0900

    Update prime_factorization.py

--- 1_functions/prime_factorization.py
+++ 1_functions/prime_factorization.py
@@ -1,8 +1,8 @@
 # prime factorization (O(sqrt(N)))
 def trial_division(n):
     factor=[]
-    sqrt=int(n**.5)+1 # sqrt=ceil(sqrt(n))
-    for d in range(2,sqrt):
+    sqrt=int(n**.5) # sqrt=ceil(sqrt(n))
+    for d in range(2,sqrt+1):
         while(n%d==0):
             n//=d
             factor.append(d)
