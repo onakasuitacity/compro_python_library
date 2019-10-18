0898d6fed7facdd9c4e5838542c312199d168878
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-18 19:00:14 +0900

    Update prime_factorization.py

--- 1_functions/prime_factorization.py
+++ 1_functions/prime_factorization.py
@@ -1,10 +1,10 @@
 # prime factorization (O(sqrt(N)))
-def trial_division(n):
+def prime_factorization(n):
     factor=[]
     sqrt=int(n**.5) # sqrt=ceil(sqrt(n))
     for d in range(2,sqrt+1):
         while(n%d==0):
             n//=d
             factor.append(d)
-    if n==1: return factor
-    else: return factor+[n]
+    if n!=1: factor.append(n)
+    return factor
