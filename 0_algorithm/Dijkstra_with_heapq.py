d80ab38fc195135e4484b6b882c32975e78c5d0d
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-18 17:19:28 +0900

    Update Dijkstra_with_heapq.py

--- 0_algorithm/Dijkstra_with_heapq.py
+++ 0_algorithm/Dijkstra_with_heapq.py
@@ -1,38 +1,33 @@
-# Dijkstra's algorithm with heap tree (O(ElogV))
-# https://takeg.hatenadiary.jp/entry/2019/06/03/130418
-class Dijkstra(object):
-    def __init__(self,E,start=0):
-        """
-        E: adjacency list with weight
-        """
-        self.__dist=[float("inf")]*len(E)
-        self.__dist[start]=0
-        self.__calculate(E,start)
-
-    @property
-    def dist(self):
-        return self.__dist
-    
-    def __calculate(self,E,start):
-        import heapq
-        dist=self.__dist
-        Q=[(0,start)] # (dist,node)
-        while(Q):
-            d,v=heapq.heappop(Q)
-            if(dist[v]<d): continue # 候補として挙がったd,vだが、他に短いのがある
-            for u,w in E[v]:
-                if(dist[u]>dist[v]+w):
-                    dist[u]=dist[v]+w
-                    heapq.heappush(Q,(dist[u],u))
-
-# example
-V=list(range(5))
+# Dijkstra with heap tree (O(ElogV))
+# https://qiita.com/shizuma/items/e08a76ab26073b21c207
+INF=float("inf")
+n=5
+u=0
 E=[
-[[1,50],[2,80]],
-[[2,20],[3,15]],
-[[3,10],[4,15]],
-[[4,30]],
+[(1,50),(2,80)],
+[(2,20),(3,15)],
+[(3,10),(4,15)],
+[(4,30)],
 []
 ]
-dij=Dijkstra(E,0)
-print(dij.dist) # [0,50,70,65,85]
+import heapq
+Q=[(0,u)] # (d,v)
+dist=[INF]*n
+dist[u]=0
+prev=[-1]*n
+while(Q):
+    d,v=heapq.heappop(Q)
+    if(dist[v]<d): continue
+    for nv,w in E[v]:
+        if(dist[nv]>dist[v]+w):
+            dist[nv]=dist[v]+w
+            prev[nv]=v
+            heapq.heappush(Q,(dist[nv],nv))
+
+# path restoration (O(N))
+v=4
+path=[]
+while(v!=-1):
+    path.append(v)
+    v=prev[v]
+print(path[::-1])
