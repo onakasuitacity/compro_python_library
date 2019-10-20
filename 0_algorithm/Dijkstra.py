abaea6e79528890573c0ba8d771ab2b92b6aa89d
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-20 18:13:04 +0900

    Update Dijkstra.py

--- 0_algorithm/Dijkstra.py
+++ 0_algorithm/Dijkstra.py
@@ -1,43 +1,28 @@
-# Dijkstra's algorithm O(V^2)
+# pure Dijkstra's algorithm (O(V^2))
 # https://qiita.com/shizuma/items/e08a76ab26073b21c207
-class Dijkstra(object):    
-    def __init__(self,E,start=0):
-        """
-        E: adjacency list with weight
-        """
-        self.__V=list(range(len(E)))
-        self.__dist=[float("inf")]*len(E)
-        self.__dist[start]=0
-        self.__prev=[None]*len(E)
-        self.__calculate(E,start)
-
-    @property
-    def dist(self):
-        return self.__dist
-
-    @property
-    def prev(self):
-        return self.__prev
-    
-    def __calculate(self,E,start):
-        dist=self.__dist
-        Q=set(self.__V)
-        while(Q):
-            v=min((self.dist[v],v) for v in Q)[1]
-            Q.remove(v)
-            for u,w in E[v]:
-                if(dist[u]>dist[v]+w):
-                    dist[u]=dist[v]+w
-                    self.__prev[u]=v
-                    
-# example
-V=list(range(5))
+n=5
+INF=float("inf")
+u=0 # start (パラメータで持つ必要はなく、distを0で初期化するだけ)
 E=[
 [[1,50],[2,80]],
 [[2,20],[3,15]],
 [[3,10],[4,15]],
 [[4,30]],
 []
+]
+
+dist=[INF]*n; dist[u]=0
+prev=[-1]*n
+calculated=[False]*n
+for _ in range(n-1):
+    v=min((dist[v],v) for v in range(n) if(not calculated[v]))[1]
+    calculated[v]=True
+    for nv,w in E[v]:
+        if(dist[nv]>dist[v]+w):
+            dist[nv]=dist[v]+w
+            prev[nv]=v
+print(dist) # [0,50,70,65,85]
+print(prev) # [-1,0,1,1,2]
 ]
 start=0
 
