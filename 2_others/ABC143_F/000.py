0866d999ff28595dad62652c019af2f39a546117
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-22 04:31:53 +0900

    Update 000.py

--- 2_others/ABC143_F/000.py
+++ 2_others/ABC143_F/000.py
@@ -7,11 +7,11 @@ A=[4,6,7,8,1,2,110,2,4,12,3,9]
 x=25
 
 # preparation
-from bisect import bisect_right
 S=[0]*(n+1)
 for i in range(n):
     S[i+1]=S[i]+A[i]
  
 # calculate
+from bisect import bisect_right
 for l in range(n):
     print(l,bisect_right(S,x+S[l])-1)
