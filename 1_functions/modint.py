8de3321933f0477f31727be31cdd60d58a4ca90c
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-24 22:48:35 +0900

    Update modint.py

--- 1_functions/modint.py
+++ 1_functions/modint.py
@@ -1,6 +1,6 @@
 # modint
 # https://qiita.com/wotsushi/items/c936838df992b706084c
-class ModInt(object):
+class mint(object):
     MOD=998244353
 
     def __init__(self,x):
@@ -10,29 +10,29 @@ class ModInt(object):
         return str(self.__x)
 
     def __add__(self,other):
-        return ModInt(self.__x+other.__x) if isinstance(other,ModInt) else ModInt(self.__x+other)
+        return mint(self.__x+other.__x) if isinstance(other,mint) else mint(self.__x+other)
 
     def __sub__(self,other):
-        return ModInt(self.__x-other.__x) if isinstance(other,ModInt) else ModInt(self.__x-other)
+        return mint(self.__x-other.__x) if isinstance(other,mint) else mint(self.__x-other)
 
     def __mul__(self,other):
-        return ModInt(self.__x*other.__x) if isinstance(other,ModInt) else ModInt(self.__x*other)
+        return mint(self.__x*other.__x) if isinstance(other,mint) else mint(self.__x*other)
 
     def __truediv__(self,other):
-        return ModInt(self.__x*pow(other.__x,self.MOD-2,self.MOD)) if isinstance(other, ModInt) else ModInt(self.__x*pow(other, self.MOD-2,self.MOD))
+        return mint(self.__x*pow(other.__x,self.MOD-2,self.MOD)) if isinstance(other, mint) else mint(self.__x*pow(other, self.MOD-2,self.MOD))
 
     def __pow__(self,other):
-        return ModInt(pow(self.__x,other.__x,self.MOD)) if isinstance(other,ModInt) else ModInt(pow(self.__x,other,self.MOD))
+        return mint(pow(self.__x,other.__x,self.MOD)) if isinstance(other,mint) else mint(pow(self.__x,other,self.MOD))
 
     __radd__=__add__
 
     def __rsub__(self,other):
-        return ModInt(other.__x-self.__x) if isinstance(other,ModInt) else ModInt(other-self.__x)
+        return mint(other.__x-self.__x) if isinstance(other,mint) else mint(other-self.__x)
 
     __rmul__=__mul__
 
     def __rtruediv__(self,other):
-        return ModInt(other.__x*pow(self.__x,self.MOD-2,self.MOD)) if isinstance(other,ModInt) else ModInt(other*pow(self.__x,self.MOD-2,self.MOD))
+        return mint(other.__x*pow(self.__x,self.MOD-2,self.MOD)) if isinstance(other,mint) else mint(other*pow(self.__x,self.MOD-2,self.MOD))
 
     def __rpow__(self,other):
-        return ModInt(pow(other.__x,self.__x,self.MOD)) if isinstance(other,ModInt) else ModInt(pow(other,self.__x,self.MOD))
+        return mint(pow(other.__x,self.__x,self.MOD)) if isinstance(other,mint) else mint(pow(other,self.__x,self.MOD))
