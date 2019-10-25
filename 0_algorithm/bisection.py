0a6780abc75ea194bd616ecb027fdcfd737fa1e3
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-25 20:24:49 +0900

    Update bisection.py

--- 0_algorithm/bisection.py
+++ 0_algorithm/bisection.py
@@ -1,5 +1,5 @@
 # bisection (O(log(r-l)))
-def bisection(l,r,f,discrete=True,left=True):
+def bisection(l,r,f,left=True,discrete=True):
     eps=1 if discrete else 10**-12
     if((not left)^f(r)): return r if left else r+1
     elif(left^f(l)): return l-1 if left else l
@@ -7,7 +7,7 @@ def bisection(l,r,f,discrete=True,left=True):
         h=(l+r)//2 if discrete else (l+r)/2
         if((not left)^f(h)): l=h
         else: r=h
-    return h if not discrete else l if left else r
+    return (l+r)/2 if not discrete else l if left else r
 
 # example
 print(bisect(-4.2,10.2,lambda x:x>6.5,discrete=False,left=False))
