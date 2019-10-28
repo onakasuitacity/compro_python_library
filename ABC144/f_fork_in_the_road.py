a4c807c7d60abce445effe93372ac0b26ddd85bb
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-29 08:44:41 +0900

    Update f_fork_in_the_road.py

--- ABC144/f_fork_in_the_road.py
+++ ABC144/f_fork_in_the_road.py
@@ -6,30 +6,38 @@ MOD=10**9+7
 input=lambda :sys.stdin.readline().rstrip()
 def resolve():
     n,m=map(int,input().split())
-    E=[[] for _ in range(n)]
+    edges=[[] for _ in range(n)]
     for _ in range(m):
         s,t=map(int,input().split())
         s-=1; t-=1
-        E[s].append(t)
-    dp=[0]*n
-    for v in range(n-2,-1,-1):
-        for nv in E[v]:
-            dp[v]+=dp[nv]
-        dp[v]/=len(E[v])
-        dp[v]+=1
-    ans=dp[0]
+        edges[s].append(t)
 
-    # u→nu を削除する
-    for u in range(n):
-        if(len(E[u])<=1): continue
-        nu=max(*((dp[nu],nu) for nu in E[u]))[1]
-        dp2=[0]*n
-        for v in range(n-2,-1,-1):
-            for nv in E[v]:
-                if(v==u and nv==nu): continue
-                dp2[v]+=dp2[nv]
-            dp2[v]/=(len(E[v]) if v!=u else len(E[v])-1)
-            dp2[v]+=1
-        ans=min(ans,dp2[0])
+    # 確率DP
+    p=[0]*n
+    p[0]=1.0
+    for v in range(n):
+        for nv in edges[v]:
+            p[nv]+=p[v]/len(edges[v])
+
+    # 期待値DP
+    E=[0]*n
+    for v in range(n-2,-1,-1):
+        for nv in edges[v]:
+            E[v]+=E[nv]
+        E[v]=1+E[v]/len(edges[v])
+
+    ans=E[0]
+    for v in range(n):
+        if(len(edges[v])<=1): continue
+        # 各vに対して、辺vwのうちE[w]が最大のものを削除する
+        w=max(*((E[w],w) for w in edges[v]))[1]
+        E_dash=0
+        for nv in edges[v]:
+            if(nv==w): continue
+            E_dash+=E[nv]
+        E_dash=1+E_dash/(len(edges[v])-1)
+        dE=E_dash-E[v]
+        ans=min(ans,E[0]+p[v]*dE)
+
     print(ans)
 resolve()
