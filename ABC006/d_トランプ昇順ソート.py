5fb21af0a72a7aad8313d27d1f005c012c853ca3
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-10-31 09:59:48 +0900

    Create d_トランプ昇順ソート.py

---
+++ ABC006/d_トランプ昇順ソート.py
@@ -0,0 +1,16 @@
+# 
+import sys
+sys.setrecursionlimit(2147483647)
+INF=float("inf")
+MOD=10**9+7
+input=lambda :sys.stdin.readline().rstrip()
+def resolve():
+    from bisect import bisect_left as bisect
+    n=int(input())
+    A=[int(input()) for _ in range(n)]
+    B=[INF]*n
+    for a in A:
+        i=bisect(B,a)
+        B[i]=a
+    print(n-bisect(B,n+1))
+resolve()
