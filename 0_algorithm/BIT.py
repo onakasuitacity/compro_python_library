1b985977e1b3d9b4e917e9923648cd80091ce516
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-11-07 11:49:29 +0900

    Update BIT.py

--- 0_algorithm/BIT.py
+++ 0_algorithm/BIT.py
@@ -12,13 +12,14 @@ class BIT(object):
             j=i+(i&-i)
             if(j<=n): self.__node[j]=dot(self.__node[i],self.__node[j])
 
-    def add(self,i,w):
+    def add(self,i,w=1):
         i+=1
         while(i<=self.__n):
             self.__node[i]=self.__dot(self.__node[i],w)
             i+=i&-i
 
     def sum(self,i):
+        i+=1
         res=self.__e
         while(i>0):
             res=self.__dot(res,self.__node[i])
@@ -30,26 +31,15 @@ class BIT(object):
         return self.__inv(self.sum(r),self.sum(l))
 
     def bisect_left(self,w,increase=True):
-        if(increase):
-            if(w>self.sum(self.__n)): return self.__n
-            n=2**((self.__n-1).bit_length())
-            res=0
-            while(n>0):
-                if(res+n<=self.__n and w>self.__node[res+n]):
-                    w-=self.__node[res+n]
-                    res+=n
-                n//=2
-            return res
-        else:
-            if(w<self.sum(self.__n)): return self.__n
-            n=2**((self.__n-1).bit_length())
-            res=0
-            while(n>0):
-                if(res+n<=self.__n and w<self.__node[res+n]):
-                    w+=self.__node[res+n]
-                    res+=n
-                n//=2
-            return res
+        if(w>self.sum(self.__n-1)): return self.__n
+        n=2**((self.__n-1).bit_length())
+        res=0
+        while(n>0):
+            if(res+n<=self.__n and ((w>self.__node[res+n])^(not increase))):
+                w-=self.__node[res+n]
+                res+=n
+            n//=2
+        return res
 
 # example
 from operator import add,sub
@@ -57,7 +47,7 @@ A=[5,3,7,9,6,4,1,2]
 N=len(A)
 bit=BIT(A,add,0,sub)
 print(bit._BIT__node) # ['$',5,8,7,24,6,10,1,37]
-print([bit.sum(i) for i in range(1,N+1)]) # [5,8,15,24,30,34,35,37]
+print([bit.sum(i) for i in range(N)]) # [5,8,15,24,30,34,35,37]
 print(bit.range_sum(3,5)) # 15=sum(A[3:5])
 k=36
 print(bit.bisect_left(k)) # 4
