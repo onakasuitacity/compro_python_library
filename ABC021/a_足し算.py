b99d2202bb911ca1a84f140e8d1ca7573b948c39
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-11-09 20:43:51 +0900

    Create a_足し算.py

---
+++ ABC021/a_足し算.py
@@ -0,0 +1,12 @@
+# https://atcoder.jp/contests/abc021/tasks/abc021_a
+import sys
+sys.setrecursionlimit(2147483647)
+INF=float("inf")
+MOD=10**9+7
+input=lambda :sys.stdin.readline().rstrip()
+def resolve():
+    n=int(input())
+    print(n)
+    for _ in range(n):
+        print(1)
+resolve()
