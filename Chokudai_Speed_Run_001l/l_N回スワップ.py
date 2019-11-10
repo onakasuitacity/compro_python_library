42c0b955bc7a0d4395b3405d23d6914d57eb6a12
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-11-10 22:58:34 +0900

    Create l_N回スワップ.py

---
+++ Chokudai_Speed_Run_001l/l_N回スワップ.py
@@ -0,0 +1,26 @@
+# https://atcoder.jp/contests/chokudai_s001/tasks/chokudai_S001_l
+import sys
+sys.setrecursionlimit(2147483647)
+INF=float("inf")
+MOD=10**9+7
+input=lambda :sys.stdin.readline().rstrip()
+def resolve():
+    n=int(input())
+    perm=list(map(lambda x:int(x)-1,input().split()))
+
+    visited=[0]*n
+    def dfs(v)->bool:
+        if(visited[v]): return False
+        visited[v]=1
+        Q=[v]
+        while(Q):
+            v=Q.pop()
+            nv=perm[v]
+            if(visited[nv]): break
+            visited[nv]=1
+            Q.append(nv)
+        return True
+
+    cnt=sum(dfs(v) for v in range(n))
+    print("YES" if(cnt%2==0) else "NO")
+resolve()
