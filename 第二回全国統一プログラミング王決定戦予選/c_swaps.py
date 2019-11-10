b53c3d1287f25d91d67e36bc74dae66d1ebde85e
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-11-10 12:14:45 +0900

    Update c_swaps.py

--- 第二回全国統一プログラミング王決定戦予選/c_swaps.py
+++ 第二回全国統一プログラミング王決定戦予選/c_swaps.py
@@ -8,38 +8,32 @@ def resolve():
     n=int(input())
     A=list(map(int,input().split()))
     B=list(map(int,input().split()))
-    X=[(a,b) for a,b in zip(A,B)]
+    X=list(zip(A,B))
     X.sort(lambda x:x[1])
 
-    # B を先に昇順ソートしておき、それに応じて A の順序を定める
-    A=[X[i][0] for i in range(n)]
-    B=[X[i][1] for i in range(n)]
-    # Aのソート結果のインデックスを保持する
-    C=[(a,i) for i,a in enumerate(A)]
+    C=[(x[0],i) for i,x in enumerate(X)]
     C.sort()
+    perm=[C[i][1] for i in range(n)]
 
-    # 最適な場合でも不可能なときはNo
-    if(any(C[i][0]>B[i] for i in range(n))):
+    A.sort()
+    B.sort()
+
+    # A <= B を満たしていなければ不可能
+    if(any(A[i]>B[i] for i in range(n))):
         print("No")
         return
 
-    # 自由度が1つ以上あればYes
-    if(any(C[i+1][0]<=B[i] for i in range(n-1))):
+    # 自由度が1つあれば可能
+    if(any(A[i+1]<=B[i] for i in range(n-1))):
         print("Yes")
         return
 
-    # この時点で A に値の重複は無いので、A のソートのサイクルを見る
-    I=[C[i][1] for i in range(n)]
-    x=0
+    # permのYoung図が[n]かどうかで判断
+    p=0
     cnt=0
     while(True):
         cnt+=1
-        x=I[x]
-        if(x==0): break
-
-    # サイクルが1つの場合は不可能、それ以外は可能
-    if(cnt<n):
-        print("Yes")
-    else:
-        print("No")
+        p=perm[p]
+        if(p==0): break
+    print("Yes" if(cnt<n) else "No")
 resolve()
