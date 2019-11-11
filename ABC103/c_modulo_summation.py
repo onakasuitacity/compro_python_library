c0cc469b4220b60ac712b27c4d00343af596e3d6
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-11-12 01:15:30 +0900

    Create c_modulo_summation.py

---
+++ ABC103/c_modulo_summation.py
@@ -0,0 +1,11 @@
+# https://atcoder.jp/contests/abc103/tasks/abc103_c
+import sys
+sys.setrecursionlimit(2147483647)
+INF=float("inf")
+MOD=10**9+7
+input=lambda :sys.stdin.readline().rstrip()
+def resolve():
+    n=int(input())
+    A=list(map(int,input().split()))
+    print(sum(A)-n)
+resolve()
