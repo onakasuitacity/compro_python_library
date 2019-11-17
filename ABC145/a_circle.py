5da59e96311a39c7476cecf83de79769a136d7ff
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-11-17 18:42:02 +0900

    Create a_circle.py

---
+++ ABC145/a_circle.py
@@ -0,0 +1,10 @@
+# https://atcoder.jp/contests/abc145/tasks/abc145_a
+import sys
+sys.setrecursionlimit(2147483647)
+INF=float("inf")
+MOD=10**9+7
+input=lambda :sys.stdin.readline().rstrip()
+def resolve():
+    r=int(input())
+    print(r*r)
+resolve()
