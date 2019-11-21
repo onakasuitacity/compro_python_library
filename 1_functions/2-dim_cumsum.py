4706161c22cac122766d11a8aa360c1189dc44aa
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-11-21 18:43:29 +0900

    Update 2-dim_cumsum.py

--- 1_functions/2-dim_cumsum.py
+++ 1_functions/2-dim_cumsum.py
@@ -1,24 +1,29 @@
 # 2-dim cumulative sum
-class cumsum_2D(object):
-    def __init__(self,A):
-        "A: 2-dim matrix"
-        S=[a[:] for a in A] # deep copy
-        from itertools import product
-        for i,j in product(range(len(A)),range(len(A[0]))):
-            if(i): S[i][j]+=S[i-1][j]
-            if(j): S[i][j]+=S[i][j-1]
-            if(i and j): S[i][j]-=S[i-1][j-1]
-        self.__S=S
+class cumsum2(object):
+    def __init__(self,m,n):
+        self.__m=m
+        self.__n=n
+        self.__S=[[0]*(n+1) for _ in range(m+1)]
 
-    @property
-    def cumsum(self):
-        return self.__S
-        
-    def sum(self,l1,r1,l2,r2):
-        if(l1>=r1 or l2>=r2): return 0
+    def add(self,i,j,w):
+        self.__S[i+1][j+1]+=w
+
+    def cumurate(self):
         S=self.__S
-        res=S[r1-1][r2-1]
-        if(l1): res-=S[l1-1][r2-1]
-        if(l2): res-=S[r1-1][l2-1]
-        if(l1 and l2): res+=S[l1-1][l2-1]
-        return res
+        for i in range(self.__m):
+            for j in range(self.__n):
+                S[i+1][j+1]+=S[i+1][j]+S[i][j+1]-S[i][j]
+
+    def sum(self,i0,i1,j0,j1):
+        S=self.__S
+        return S[i1][j1]-S[i0][j1]-S[i1][j0]+S[i0][j0]
+
+C=cumsum2(2,3)
+C.add(0,0,1)
+C.add(0,1,2)
+C.add(0,2,3)
+C.add(1,0,4)
+C.add(1,1,5)
+C.add(1,2,6)
+C.cumurate()
+print(C.sum(0,2,1,3))
