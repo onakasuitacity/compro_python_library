97d2f02a363305b7744eba2f83a9e651d2b50847
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-11-29 06:26:00 +0900

    Update rolling_hash.py

--- 0_algorithm/rolling_hash.py
+++ 0_algorithm/rolling_hash.py
@@ -2,25 +2,16 @@
 # https://ei1333.github.io/luzhiled/snippets/string/rolling-hash.html
 # http://perogram.hateblo.jp/entry/rolling_hash
 class RollingHash(object):
-    """
-    construct: O(N)
-    query:
-        hash: O(1)
-        lcp: O(logN)
-        search: O(N)
-    """
     __base1=1007; __mod1=10**9+9
     __base2=1009; __mod2=10**9+7
 
     def __init__(self,s):
-        n=len(s)
-        self.__s=s
-        self.__n=n
+        self.__s=s; self.__n=len(s)
         b1=self.__base1; m1=self.__mod1
         b2=self.__base2; m2=self.__mod2
-        H1,H2=[0]*(n+1),[0]*(n+1)
-        P1,P2=[1]*(n+1),[1]*(n+1)
-        for i in range(n):
+        H1,H2=[0]*(self.__n+1),[0]*(self.__n+1)
+        P1,P2=[1]*(self.__n+1),[1]*(self.__n+1)
+        for i in range(self.__n):
             H1[i+1]=(H1[i]*b1+ord(s[i]))%m1
             H2[i+1]=(H2[i]*b2+ord(s[i]))%m2
             P1[i+1]=P1[i]*b1%m1
@@ -28,40 +19,10 @@ class RollingHash(object):
         self.__H1=H1; self.__H2=H2
         self.__P1=P1; self.__P2=P2
 
-    def hash(self,l,r=None):
-        m1=self.__mod1; m2=self.__mod2
-        if(r is None): r=self.__n
-        assert 0<=l<=r<=self.__n
-        return ((self.__H1[r]-self.__P1[r-l]*self.__H1[l]%m1)%m1,
-                (self.__H2[r]-self.__P2[r-l]*self.__H2[l]%m2)%m2)
-
-    @classmethod
-    def lcp(cls,rh1,rh2,l1,l2,r1=None,r2=None):
-        if(r1 is None): r1=rh1.__n
-        if(r2 is None): r2=rh2.__n
-        assert 0<=l1<=r1<=rh1.__n and 0<=l2<=r2<=rh2.__n
-        L=0
-        R=min(r1-l1,r2-l2)
-        if(rh1.hash(l1,l1+R)==rh2.hash(l2,l2+R)): return R
-        while(R-L>1):
-            H=(L+R)//2
-            if(rh1.hash(l1,l1+H)==rh2.hash(l2,l2+H)): L=H
-            else: R=H
-        return L
-
-    @classmethod
-    def search(cls,pattern,text):
-        n=text.__n; m=pattern.__n
-        res=[]
-        for i in range(n-m+1):
-            if(text.hash(i,i+m)==pattern.hash(0,m)):
-                res.append(i)
-        return res
-
-# example
-s="unvhusmjlvieloveuybouqvnqjygutqlovedkfsdfgheaiuloveaeiuvaygayfg"
-t="love"
-s=RollingHash(s)
-t=RollingHash(t)
-print(RollingHash.search(t,s)) # [12,31,47]
-print(RollingHash.lcp(s,t,12,0)) # 4
+    def __getitem__(self,x):
+        if(isinstance(x,int)): x=slice(x,x+1)
+        assert(isinstance(x,slice) and ((x.step is None) or (x.step==1)))
+        l=x.start; r=x.stop;
+        assert(l<=r)
+        return ((self.__H1[r]-self.__P1[r-l]*self.__H1[l]%self.__mod1)%self.__mod1,
+                (self.__H2[r]-self.__P2[r-l]*self.__H2[l]%self.__mod2)%self.__mod2)
