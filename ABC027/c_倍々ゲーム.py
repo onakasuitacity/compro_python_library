be39228ea7aa50fa48dbcc663ee472dd3bdd5ec0
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-12-01 18:03:19 +0900

    Create c_倍々ゲーム.py

---
+++ ABC027/c_倍々ゲーム.py
@@ -0,0 +1,16 @@
+# https://atcoder.jp/contests/abc027/tasks/abc027_c
+import sys
+sys.setrecursionlimit(2147483647)
+INF=float("inf")
+MOD=10**9+7
+input=lambda:sys.stdin.readline().rstrip()
+from collections import defaultdict
+def resolve():
+    n=int(input())
+    n-=1
+    r=4
+    while(n>=2*r):
+        n-=2*r
+        r*=4
+    print("Takahashi" if(1<=n<=r) else "Aoki")
+resolve()
