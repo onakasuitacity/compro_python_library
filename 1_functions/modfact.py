290984a1e76a29eb4f6c43b2ed6e2bdd80b65707
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-12-04 11:46:38 +0900

    Update modfact.py

--- 1_functions/modfact.py
+++ 1_functions/modfact.py
@@ -1,15 +1,15 @@
 MOD=10**9+7
 class modfact(object):
     def __init__(self,n):
-        fact=[1]*(n+1)
-        invfact=[1]*(n+1)
-        for i in range(1,n+1):
-            fact[i]=i*fact[i-1]%MOD
+        fact=[1]*(n+1); invfact=[1]*(n+1)
+        for i in range(1,n+1): fact[i]=i*fact[i-1]%MOD
         invfact[n]=pow(fact[n],MOD-2,MOD)
-        for i in range(n-1,-1,-1):
-            invfact[i]=invfact[i+1]*(i+1)%MOD
-        self.__fact=fact
-        self.__invfact=invfact
+        for i in range(n-1,-1,-1): invfact[i]=invfact[i+1]*(i+1)%MOD
+        self.__fact=fact; self.__invfact=invfact
+
+    def inv(self,n):
+        assert(n>0)
+        return self.__fact[n-1]*self.__invfact[n]%MOD
 
     def fact(self,n):
         return self.__fact[n]
@@ -18,5 +18,9 @@ class modfact(object):
         return self.__invfact[n]
 
     def comb(self,n,k):
-        if(k<0 or n-k<0): return 0
-        return (self.fact(n)*self.invfact(k)*self.invfact(n-k))%MOD
+        if(k<0 or n<k): return 0
+        return self.__fact[n]*self.__invfact[k]*self.__invfact[n-k]%MOD
+
+    def perm(self,n,k):
+        if(k<0 or n<k): return 0
+        self.__fact[n]*self.__invfact[k]%MOD
