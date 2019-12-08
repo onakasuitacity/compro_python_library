4b9cbd684e83398062e833743119ed1babb9cf27
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-12-08 19:05:28 +0900

    Update trisection.py

--- 0_algorithm/trisection.py
+++ 0_algorithm/trisection.py
@@ -1,11 +1,11 @@
 def trisection(l,r,f,convex=True,discrete=True):
-    eps=2 if(discrete) else 10**-12
+    eps=2 if(discrete) else 10**-8
     while(r-l>eps):
         d=(r-l)//3 if(discrete) else (r-l)/3
         h1=l+d; h2=r-d
         if((f(h1)>=f(h2))^convex): r=h2
         else: l=h1
-    if(not discrete): return (l+r)/2
+    if(not discrete): return f((l+r)/2)
     elif(convex): return min([(f(l),l),(f(l+1),l+1),(f(r),r)])[1]
     else: return max([(f(l),l),(f(l+1),l+1),(f(r),r)])[1]
 
