9fb7db4704583b65c7b51aa66e852e347e85a4df
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-12-12 20:29:50 +0900

    Update divisors.py

--- 1_functions/divisors.py
+++ 1_functions/divisors.py
@@ -2,8 +2,8 @@
 def divisors(n):
     S,T=[],[]
     for i in range(1,int(n**.5)+1):
-        if n%i==0:
+        if(n%i==0):
             S.append(i)
             T.append(n//i)
     T.reverse()
-    return S+T if S[-1]!=T[0] else S+T[1:]
+    return S+T if(S[-1]!=T[0]) else S+T[1:]
