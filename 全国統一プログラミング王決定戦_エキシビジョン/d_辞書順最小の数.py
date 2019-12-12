de587eb8fe2301b89144b8fe59529f508876bb06
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-12-12 19:09:57 +0900

    Create d_辞書順最小の数.py

---
+++ 全国統一プログラミング王決定戦_エキシビジョン/d_辞書順最小の数.py
@@ -0,0 +1,9 @@
+# https://atcoder.jp/contests/nikkei2019-ex/tasks/nikkei2019ex_d
+import sys
+sys.setrecursionlimit(2147483647)
+INF=float("inf")
+MOD=10**9+7
+input=lambda:sys.stdin.readline().rstrip()
+def resolve():
+    print(10**(int(input())-1))
+resolve()
