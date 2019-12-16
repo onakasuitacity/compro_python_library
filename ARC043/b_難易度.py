138963ec468e75d59d5ba02be76269cdabdc6ddd
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-12-17 04:26:34 +0900

    Update b_難易度.py

--- ARC043/b_難易度.py
+++ ARC043/b_難易度.py
@@ -13,17 +13,13 @@ def resolve():
     for d in D:
         dp[d]+=1
 
-    for i in range(V):
-        dp[i+1]+=dp[i]
-
     for j in range(3):
+        for i in range(V):
+            dp[i+1]+=dp[i]
         ndp=[0]*(V+1)
         for d in D:
             ndp[d]+=dp[d//2]
         dp=ndp
-        if(j!=2):
-            for i in range(V):
-                dp[i+1]+=dp[i]
 
     print(sum(dp)%MOD)
 resolve()
