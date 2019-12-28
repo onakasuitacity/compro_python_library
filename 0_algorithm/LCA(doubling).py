8de2665d9f9be629dbc0a04c01ed48cac855ad2e
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2019-12-28 10:54:56 +0900

    Update LCA(doubling).py

--- 0_algorithm/LCA(doubling).py
+++ 0_algorithm/LCA(doubling).py
@@ -4,7 +4,7 @@ class LCA(object):
     def __init__(self,E,root=0):
         self.__n=len(E); self.__E=E; self.__logn=(self.__n-1).bit_length()
         self.__depth=[-1]*self.__n; self.__depth[root]=0
-        self.__parents=[[-1]*self.__n for _ in range(self.__logn)]
+        self.__parent=[[-1]*self.__n for _ in range(self.__logn)]
         self.__dfs(root); self.__doubling()
 
     def __dfs(self,v):
@@ -13,32 +13,37 @@ class LCA(object):
             v=Q.pop()
             for nv in self.__E[v]:
                 if(self.__depth[nv]!=-1): continue
-                self.__parents[0][nv]=v
+                self.__parent[0][nv]=v
                 self.__depth[nv]=self.__depth[v]+1
                 Q.append(nv)
 
     def __doubling(self):
-        for i in range(1,self.__logn):
+        for k in range(1,self.__logn):
             for v in range(self.__n):
-                if(self.__parents[i-1][v]==-1): continue
-                self.__parents[i][v]=self.__parents[i-1][self.__parents[i-1][v]]
+                if(self.__parent[k-1][v]==-1): continue
+                self.__parent[k][v]=self.__parent[k-1][self.__parent[k-1][v]]
 
-    @property
-    def depth(self):
-        return self.__depth
+    def depth(self,v):
+        return self.__depth[v]
+
+    def kth_parent(self,k,v):
+        return self.__parent[k][v]
+
+    def dist(self,u,v):
+        return self.__depth[u]+self.__depth[v]-2*self.__depth[self.get(u,v)]
 
     def get(self,u,v):
         dd=self.__depth[v]-self.__depth[u]
-        if(dd<0): # vの方が深いようにする
+        if(dd<0):
             u,v=v,u
             dd*=-1
-        for i in range(self.__logn):
-            if(dd&(1<<i)): v=self.__parents[i][v]
+        for k in range(self.__logn):
+            if((dd>>k)&1): v=self.__parent[k][v]
         if(v==u): return v
-        for i in range(self.__logn-1,-1,-1):
-            pu,pv=self.__parents[i][u],self.__parents[i][v]
+        for k in range(self.__logn-1,-1,-1):
+            pu,pv=self.__parent[k][u],self.__parent[k][v]
             if(pu!=pv): u,v=pu,pv
-        return self.__parents[0][u]
+        return self.__parent[0][u]
 
 # example
 N=6
