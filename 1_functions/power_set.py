663b557efe8178631ef21e355cc926cd94616bf4
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2020-05-04 16:59:01 +0900

    Delete power_set.py

--- 1_functions/power_set.py
+++
@@ -1,7 +0,0 @@
-# power set
-# https://qiita.com/hrsma2i/items/d98f9b1fceeb23d67eef
-from itertools import chain,combinations
-def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
