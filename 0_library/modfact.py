99dc183858260db58a3a71b8691bfdec77430d80
moni <54064820+onakasuitacity@users.noreply.github.com>
2020-05-06 00:49:37 +0900

    Update modfact.py

--- 0_library/modfact.py
+++ 0_library/modfact.py
@@ -5,21 +5,21 @@ class modfact(object):
         for i in range(1, n + 1): fact[i] = i * fact[i - 1] % MOD
         invfact[n] = pow(fact[n], MOD - 2, MOD)
         for i in range(n - 1, -1, -1): invfact[i] = invfact[i + 1] * (i + 1) % MOD
-        self.fact, self.invfact = fact, invfact
+        self._fact, self._invfact = fact, invfact
 
     def inv(self, n):
-        return self.fact[n-1] * self.invfact[n] % MOD
+        return self._fact[n-1] * self._invfact[n] % MOD
 
     def fact(self, n):
-        return self.fact[n]
+        return self._fact[n]
 
     def invfact(self, n):
-        return self.invfact[n]
+        return self._invfact[n]
 
     def comb(self, n, k):
         if k < 0 or n < k: return 0
-        return self.fact[n] * self.invfact[k] % MOD * self.invfact[n - k] % MOD
+        return self._fact[n] * self._invfact[k] % MOD * self._invfact[n - k] % MOD
 
     def perm(self, n, k):
         if k < 0 or n < k: return 0
-        return self.fact[n] * self.invfact[n - k] % MOD
+        return self._fact[n] * self._invfact[n - k] % MOD
