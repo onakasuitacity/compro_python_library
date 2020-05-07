c0a16dc89600fc351eb5d83a78c4ab86c1980f1a
moni <54064820+onakasuitacity@users.noreply.github.com>
2020-05-07 13:56:16 +0900

    Update sliding_window_aggregation.py

--- 0_library/sliding_window_aggregation.py
+++ 0_library/sliding_window_aggregation.py
@@ -1,35 +1,35 @@
 # https://scrapbox.io/data-structures/Sliding_Window_Aggregation
 class SWAG(object):
     def __init__(self, dot):
-        self.front, self.back, self.dot = [], [], dot
+        self._front, self._back, self._dot = [], [], dot
 
     def __bool__(self):
-        return bool(self.front) or bool(self.back)
+        return bool(self._front) or bool(self._back)
 
     def __len__(self):
-        return len(self.front) + len(self.back)
+        return len(self._front) + len(self._back)
 
     def append(self, val):
-        if not self.back: self.back.append((val, val))
-        else: self.back.append((val, self.dot(self.back[-1][1], val)))
+        if not self._back: self._back.append((val, val))
+        else: self._back.append((val, self._dot(self._back[-1][1], val)))
 
     def popleft(self):
-        if not self.front:
-            self.front.append((self.back[-1][0], self.back[-1][0]))
-            self.back.pop()
-            while self.back:
-                self.front.append((self.back[-1][0], self.dot(self.back[-1][0], self.front[-1][1])))
-                self.back.pop()
-        return self.front.pop()[0]
+        if not self._front:
+            self._front.append((self._back[-1][0], self._back[-1][0]))
+            self._back.pop()
+            while self._back:
+                self._front.append((self._back[-1][0], self._dot(self._back[-1][0], self._front[-1][1])))
+                self._back.pop()
+        return self._front.pop()[0]
 
     def sum(self):
-        if not self.front: return self.back[-1][1]
-        elif not self.back: return self.front[-1][1]
-        else: return self.dot(self.front[-1][1], self.back[-1][1])
+        if not self._front: return self._back[-1][1]
+        elif not self._back: return self._front[-1][1]
+        else: return self._dot(self._front[-1][1], self._back[-1][1])
 
 # example
 from operator import add
-swag=SWAG(add)
+swag = SWAG(add)
 swag.append(1)
 swag.append(2)
 swag.append(3)
