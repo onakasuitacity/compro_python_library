22f91f5a49d9715ed3664a02d3e268133791af92
moni <54064820+onakasuitacity@users.noreply.github.com>
2020-09-29 19:50:09 +0900

    Delete primes.py

--- 0_library/primes.py
+++
@@ -1,9 +0,0 @@
-# Eratosthenes Sieve (O(nloglogn))
-def primes(n):
-    if n <= 1: return []
-    S = [1] * (n + 1)
-    S[0] = S[1] = 0
-    for i in range(2, n):
-        if S[i] == 0: continue
-        for j in range(2 * i, n + 1, i): S[j] = 0
-    return [p for p in range(n + 1) if(S[p])]
