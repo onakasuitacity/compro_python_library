7419b75a3e07cf6ffad507bbaa260bfef1d79f60
moni <54064820+onakasuitacity@users.noreply.github.com>
2020-10-09 10:01:48 +0900

    Update Dinic.py

--- 0_library/Dinic.py
+++ 0_library/Dinic.py
@@ -6,18 +6,17 @@ class MaxFlow(object):
     def __init__(self, n):
         self.n = n
         self.E = [[] for _ in range(n)]
- 
+
     def add_edge(self, u, v, cap):
         e = [v, cap, 0]
         rev = [u, 0, e]
         e[2] = rev
         self.E[u].append(e)
         self.E[v].append(rev)
- 
+
     def _bfs(self, s, t):
         self._level = level = [-1] * self.n
         level[s] = 0
-        self._iter = [0] * self.n
         E = self.E
         queue = [s]
         for v in queue:
@@ -26,32 +25,32 @@ class MaxFlow(object):
                     level[nv] = level[v] + 1
                     queue.append(nv)
         return level[t] != -1
- 
+
     def _dfs(self, s, t):
         E, level, it = self.E, self._level, self._iter
-        stack = [s]
+        stack = [(s, INF)]
         while stack:
-            v = stack[-1]
+            v, f = stack[-1]
             if v == t:
-                f = min(E[v][it[v]][1] for v in stack[:-1])
-                for v in stack[:-1]:
+                for v, _ in stack[:-1]:
                     E[v][it[v]][1] -= f
                     E[v][it[v]][2][1] += f
                 return f
             while it[v] < len(E[v]):
                 nv, cap, _ = E[v][it[v]]
                 if cap and level[v] < level[nv]:
-                    stack.append(nv)
+                    stack.append((nv, min(f, cap)))
                     break
                 it[v] += 1
             else:
                 stack.pop()
                 level[v] = 0
         return 0
- 
+
     def solve(self, s, t):
         res = 0
         while self._bfs(s, t):
+            self._iter = [0] * self.n
             f = 1
             while f:
                 f = self._dfs(s, t)
