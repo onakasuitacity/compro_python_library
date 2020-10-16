cfa370b13de63a6490b4af3571f086c9b088e80d
moni <54064820+onakasuitacity@users.noreply.github.com>
2020-10-16 10:59:32 +0900

    Update z-algorithm.py

--- 0_library/z-algorithm.py
+++ 0_library/z-algorithm.py
@@ -1,4 +1,3 @@
-# Z-algorithm (O(N))
 # https://deve68.hatenadiary.org/entry/20120201/1328109890
 # https://snuke.hatenablog.com/entry/2014/12/03/214243
 def Z(S):
@@ -7,13 +6,11 @@ def Z(S):
     res[0] = n
     l = r = 0
     for i in range(1, n):
-        if res[i - l] < r - i: # 過去の結果が全て使える
+        if res[i - l] < r - i:
             res[i] = res[i - l]
         else:
             l = i
-            r = max(i, r) # i < r ならば過去の結果が一部使える
+            r = max(i, r)
             while r < n and S[r - l] == S[r]: r += 1
             res[i] = r - l
     return res
-
-print(Z("abaaabaabb")) # [10, 0, 1, 1, 4, 0, 1, 2, 0, 0]
