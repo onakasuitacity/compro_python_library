63e37edf0e1718c933af14ab0d2304b370b7b623
moni <54064820+onakasuitacity@users.noreply.github.com>
2020-10-21 22:19:51 +0900

    Update prime_factorization.py

--- 0_library/prime_factorization.py
+++ 0_library/prime_factorization.py
@@ -1,9 +1,10 @@
-# prime factorization (O(sqrt(N)))
+from collections import Counter
 def prime_factorization(n):
-    factor = []
-    for d in range(2, int(n ** .5) + 1):
-        while n % d == 0:
-            n //= d
-            factor.append(d)
-    if n != 1: factor.append(n)
+    factor = Counter()
+    for p in range(2, int(n**.5) + 1):
+        while n % p == 0:
+            n //= p
+            factor[p] += 1
+    if n != 1:
+        factor[n] += 1
     return factor
