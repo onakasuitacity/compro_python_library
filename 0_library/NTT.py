e8db0f7d395ed44f32e1788e500840e51035cef3
moni <54064820+onakasuitacity@users.noreply.github.com>
2020-10-25 17:07:19 +0900

    Update NTT.py

--- 0_library/NTT.py
+++ 0_library/NTT.py
@@ -1,7 +1,7 @@
 prime = 998244353
 root = 3
-def _fmt(A, inverse = False):
-    N = len(A)
+def _fmt(f, inverse = False):
+    N = len(f)
     logN = (N - 1).bit_length()
     base = pow(root, (prime - 1) // N * (1 - 2 * inverse) % (prime - 1), prime)
     step = N
@@ -9,15 +9,15 @@ def _fmt(A, inverse = False):
         step >>= 1
         w = pow(base, step, prime)
         wj = 1
-        nA = [0] * N
+        nf = [0] * N
         for j in range(1 << k):
             for i in range(1 << logN - k - 1):
-                s, t = i + j * step, i + j * step + (N >> 1)
-                ps, pt = i + j * step * 2, i + j * step * 2 + step
-                nA[s], nA[t] = (A[ps] + A[pt] * wj) % prime, (A[ps] - A[pt] * wj) % prime
+                s, t = i + 2 * j * step, i + (2 * j + 1) * step
+                ns, nt = i + j * step, i + j * step + (N >> 1)
+                nf[ns], nf[nt] = (f[s] + f[t] * wj) % prime, (f[s] - f[t] * wj) % prime
             wj = (wj * w) % prime
-        A = nA
-    return A
+        f = nf
+    return f
  
 def convolution(f, g):
     N = 1 << (len(f) + len(g) - 2).bit_length()
