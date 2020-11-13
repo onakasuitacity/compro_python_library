8addfb44543c7e3003efb4f17bbdbcb167bf2551
moni <54064820+onakasuitacity@users.noreply.github.com>
2020-11-13 18:38:43 +0900

    Update sliding_window_aggregation.py

--- 0_library/sliding_window_aggregation.py
+++ 0_library/sliding_window_aggregation.py
@@ -10,29 +10,26 @@ class SWAG(object):
         return len(self._front) + len(self._back)
 
     def append(self, val):
-        if not self._back: self._back.append((val, val))
-        else: self._back.append((val, self._dot(self._back[-1][1], val)))
+        if self._back:
+            self._back.append((val, self._dot(self._back[-1][1], val)))
+        else:
+            self._back.append((val, val))
 
     def popleft(self):
+        assert self
         if not self._front:
-            self._front.append((self._back[-1][0], self._back[-1][0]))
-            self._back.pop()
+            l = self._back.pop()[0]
+            self._front.append((l, l))
             while self._back:
-                self._front.append((self._back[-1][0], self._dot(self._back[-1][0], self._front[-1][1])))
-                self._back.pop()
+                l = self._back.pop()[0]
+                self._front.append((l, self._dot(l, self._front[-1][1])))
         return self._front.pop()[0]
 
     def sum(self):
-        if not self._front: return self._back[-1][1]
-        elif not self._back: return self._front[-1][1]
-        else: return self._dot(self._front[-1][1], self._back[-1][1])
-
-# example
-from operator import add
-swag = SWAG(add)
-swag.append(1)
-swag.append(2)
-swag.append(3)
-print(swag.popleft())
-print(swag.sum())
-print(len(swag))
+        assert self
+        if not self._back:
+            return self._front[-1][1]
+        elif not self._front:
+            return self._back[-1][1]
+        else:
+            return self._dot(self._front[-1][1], self._back[-1][1])
