74715b3daab0dea5f0a8f9b1666cbe0c38ad1872
moni <54064820+onakasuitacity@users.noreply.github.com>
2020-11-27 23:38:59 +0900

    Update segment_tree.py

--- 0_library/segment_tree.py
+++ 0_library/segment_tree.py
@@ -3,14 +3,10 @@ class SegmentTree(object):
     def __init__(self, A, dot, unit):
         n = 1 << (len(A) - 1).bit_length()
         tree = [unit] * (2 * n)
-        for i, v in enumerate(A):
-            tree[i + n] = v
+        tree[n : n + len(A)] = A
         for i in range(n - 1, 0, -1):
             tree[i] = dot(tree[i << 1], tree[i << 1 | 1])
-        self._n = n
-        self._tree = tree
-        self._dot = dot
-        self._unit = unit
+        self._n, self._tree, self._dot, self._unit = n, tree, dot, unit
 
     def __getitem__(self, i):
         return self._tree[i + self._n]
@@ -25,14 +21,14 @@ class SegmentTree(object):
     def sum(self, l, r):
         l += self._n
         r += self._n
-        l_val = r_val = self._unit
+        lv = rv = self._unit
         while l < r:
             if l & 1:
-                l_val = self._dot(l_val, self._tree[l])
+                lv = self._dot(lv, self._tree[l])
                 l += 1
             if r & 1:
                 r -= 1
-                r_val = self._dot(self._tree[r], r_val)
+                rv = self._dot(self._tree[r], rv)
             l >>= 1
             r >>= 1
-        return self._dot(l_val, r_val)
+        return self._dot(lv, rv)
