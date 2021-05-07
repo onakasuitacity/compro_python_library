4439ac168826bcd8149cacf821a9cee210482d40
moni <54064820+onakasuitacity@users.noreply.github.com>
2021-05-07 21:50:40 +0900

    Update segment_tree_with_lazy_propagation.py

--- 0_library/segment_tree_with_lazy_propagation.py
+++ 0_library/segment_tree_with_lazy_propagation.py
@@ -3,13 +3,13 @@
 # http://beet-aizu.hatenablog.com/entry/2017/12/01/225955
 # http://sugarknri.hatenablog.com/entry/2018/04/16/123016
 class LazySegmentTree(object):
-    def __init__(self, A, dot, unit, compose, identity, act):
+    def __init__(self, A, dot, e, compose, id, act):
         n = len(A)
-        tree = [unit] * n + A
+        tree = [e] * n + A
         for i in range(n - 1, 0, -1):
             tree[i] = dot(tree[i << 1], tree[i << 1 | 1])
-        self._n, self._tree, self._lazy = n, tree, [identity] * 2 * n
-        self._dot, self._unit, self._compose, self._identity, self._act = dot, unit, compose, identity, act
+        self._n, self._tree, self._lazy = n, tree, [id] * 2 * n
+        self._dot, self._e, self._compose, self._id, self._act = dot, e, compose, id, act
 
     def _ascend(self, i):
         tree, lazy, dot, act = self._tree, self._lazy, self._dot, self._act
@@ -18,19 +18,18 @@ class LazySegmentTree(object):
             tree[i] = act(lazy[i], dot(tree[i << 1], tree[i << 1 | 1]))
 
     def _descend(self, i):
-        tree, lazy, identity, compose, act = self._tree, self._lazy, self._identity, self._compose, self._act
+        tree, lazy, id, compose, act = self._tree, self._lazy, self._id, self._compose, self._act
         for k in range(i.bit_length() - 1, 0, -1):
             p = i >> k
             f = lazy[p]
             tree[p << 1], lazy[p << 1] = act(f, tree[p << 1]), compose(f, lazy[p << 1])
             tree[p << 1 | 1], lazy[p << 1 | 1] = act(f, tree[p << 1 | 1]), compose(f, lazy[p << 1 | 1])
-            lazy[p] = identity
+            lazy[p] = id
 
-    def range_act(self, l, r, f):
+    def act(self, l, r, f):
         l += self._n
         r += self._n
-        # propagation isn't necessary if compose is commutative
-        self._descend(l), self._descend(r - 1)
+        self._descend(l), self._descend(r - 1)  # unnecessary when compose is commutative
         l0, r0 = l, r
         tree, lazy, act, compose = self._tree, self._lazy, self._act, self._compose
         while l < r:
@@ -47,17 +46,16 @@ class LazySegmentTree(object):
     def sum(self, l, r):
         l += self._n
         r += self._n
-        self._descend(l)
-        self._descend(r - 1)
-        l_val = r_val = self._unit
+        self._descend(l), self._descend(r - 1)
+        lv = rv = self._e
         tree, dot = self._tree, self._dot
         while l < r:
             if l & 1:
-                l_val = dot(l_val, tree[l])
+                lv = dot(lv, tree[l])
                 l += 1
             if r & 1:
                 r -= 1
-                r_val = dot(tree[r], r_val)
+                rv = dot(tree[r], rv)
             l >>= 1
             r >>= 1
-        return dot(l_val, r_val)
+        return dot(lv, rv)
