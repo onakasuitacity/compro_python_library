fa7cb780032ac45086cf753099e5c28ed566c9c6
moni <54064820+onakasuitacity@users.noreply.github.com>
2021-05-10 18:14:18 +0900

    Update lowest_common_ancestor.py

--- 0_library/lowest_common_ancestor.py
+++ 0_library/lowest_common_ancestor.py
@@ -1,74 +1,43 @@
 # LCA with doubling (O(NlogN),O(logN))
 # https://tjkendev.github.io/procon-library/python/graph/lca-doubling.html
 class LCA(object):
-    def __init__(self, E, root = 0):
-        self._n = len(E)
+    def __init__(self, G, root=0):
+        self._n = len(G)
         self._logn = (self._n - 1).bit_length()
-        self._parent = [None] * self._logn
-        self._dfs(E, root)
+        self.depth = [0] * self._n
+        self.parents = []
+        self._bfs(G, root)
         self._doubling()
 
-    def _dfs(self, E, root):
-        parent = [-1] * self._n
-        depth = [0] * self._n
-        stack = [(root, -1)]
-        while stack:
-            v, p = stack.pop()
-            for nv in E[v]:
+    def _bfs(self, G, root):
+        parent = [-1] * (self._n + 1)
+        queue = [(root, -1)]
+        for v, p in queue:
+            for nv in G[v]:
                 if nv == p:
                     continue
+                self.depth[nv] = self.depth[v] + 1
                 parent[nv] = v
-                depth[nv] = depth[v] + 1
-                stack.append((nv, v))
-        self._parent[0] = parent
-        self._depth = depth
+                queue.append((nv, v))
+        self.parents.append(parent)
 
     def _doubling(self):
-        parent = self._parent[0]
-        for k in range(1, self._logn):
-            next_parent = [-1] * self._n
-            for v in range(self._n):
-                if parent[v] == -1:
-                    continue
-                next_parent[v] = parent[parent[v]]
-            self._parent[k] = next_parent
-            parent = next_parent
+        for _ in range(self._logn - 1):
+            parent = self.parents[-1]
+            self.parents.append([parent[parent[v]] for v in range(self._n + 1)])
 
     def lca(self, u, v):
-        dd = self._depth[v] - self._depth[u]
+        dd = self.depth[v] - self.depth[u]
         if dd < 0:
             u, v = v, u
             dd *= -1
         for k in range(self._logn):
-            if (dd >> k) & 1:
-                v = self._parent[k][v]
+            if dd >> k & 1:
+                v = self.parents[k][v]
         if u == v:
             return u
         for k in range(self._logn - 1, -1, -1):
-            pu, pv = self._parent[k][u], self._parent[k][v]
+            pu, pv = self.parents[k][u], self.parents[k][v]
             if pu != pv:
                 u, v = pu, pv
-        return self._parent[0][u]
-
-    @property
-    def depth(self):
-        return self._depth
-
-    @property
-    def parent(self):
-        return self._parent
-
-# example
-N = 6
-V = list(range(N))
-E = [[1, 2], [0], [0, 3, 4, 5], [2], [3], [4]]
-
-###
-#    0
-#   / \
-#  1   2
-#     /|\
-#    3 4 5
-
-lca = LCA(E)
-lca.lca(3, 4) 
+        return self.parents[0][u]
