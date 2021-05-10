416431f4e777377f53366f8264f3b9775a06fc15
moni <54064820+onakasuitacity@users.noreply.github.com>
2021-05-10 19:33:40 +0900

    Update lowest_common_ancestor.py

--- 0_library/lowest_common_ancestor.py
+++ 0_library/lowest_common_ancestor.py
@@ -3,13 +3,12 @@
 class LCA(object):
     def __init__(self, G, root=0):
         self._n = len(G)
-        self._logn = (self._n - 1).bit_length()
         self.depth = [0] * self._n
         self.parents = []
-        self._bfs(G, root)
+        self._dfs(G, root)
         self._doubling()
 
-    def _bfs(self, G, root):
+    def _dfs(self, G, root):
         parent = [-1] * (self._n + 1)
         queue = [(root, -1)]
         for v, p in queue:
@@ -22,7 +21,7 @@ class LCA(object):
         self.parents.append(parent)
 
     def _doubling(self):
-        for _ in range(self._logn - 1):
+        for _ in range((self._n - 1).bit_length() - 1):
             parent = self.parents[-1]
             self.parents.append([parent[parent[v]] for v in range(self._n + 1)])
 
@@ -31,12 +30,12 @@ class LCA(object):
         if dd < 0:
             u, v = v, u
             dd *= -1
-        for k in range(self._logn):
+        for k in range(dd.bit_length()):
             if dd >> k & 1:
                 v = self.parents[k][v]
         if u == v:
             return u
-        for k in range(self._logn - 1, -1, -1):
+        for k in range(self.depth[u].bit_length() - 1, -1, -1):
             pu, pv = self.parents[k][u], self.parents[k][v]
             if pu != pv:
                 u, v = pu, pv
