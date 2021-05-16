4dc3c3c049e093e9b46927802e44212cf57c0d69
moni <54064820+onakasuitacity@users.noreply.github.com>
2021-05-16 15:49:29 +0900

    Update segment_tree.py

--- 0_library/segment_tree.py
+++ 0_library/segment_tree.py
@@ -1,11 +1,11 @@
 # segment tree (without lazy-propagation)
 class SegmentTree(object):
-    def __init__(self, A, dot, unit):
+    def __init__(self, A, dot, e):
         n = len(A)
-        tree = [unit] * n + A
+        tree = [e] * n + A
         for i in range(n - 1, 0, -1):
             tree[i] = dot(tree[i << 1], tree[i << 1 | 1])
-        self._n, self._tree, self._dot, self._unit = n, tree, dot, unit
+        self._n, self._tree, self._dot, self._e = n, tree, dot, e
 
     def __getitem__(self, i):
         return self._tree[i + self._n]
@@ -20,7 +20,7 @@ class SegmentTree(object):
     def sum(self, l, r):
         l += self._n
         r += self._n
-        lv = rv = self._unit
+        lv = rv = self._e
         while l < r:
             if l & 1:
                 lv = self._dot(lv, self._tree[l])
