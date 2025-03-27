08879f5d309e08897eeac9bbcf718ae158e04e91
moni <54064820+onakasuitacity@users.noreply.github.com>
2025-03-27 10:40:41 +0900

    Update sliding_window_aggregation.py

--- sliding_window_aggregation/sliding_window_aggregation.py
+++ sliding_window_aggregation/sliding_window_aggregation.py
@@ -11,63 +11,48 @@ class SWAG(object):
         return len(self._front) + len(self._back)
 
     def __getitem__(self, i):
-        if i < len(self._front):
-            return self._front[~i][0]
-        return self._back[i - len(self._front)][0]
+        return self._front[~i][0] if i < len(self._front) else self._back[i - len(self._front)][0]
 
     def __repr__(self):
         return str([self[i] for i in range(len(self))])
 
     def append(self, val):
-        if self._back:
-            self._back.append((val, self._dot(self._back[-1][1], val)))
-        else:
-            self._back.append((val, val))
+        prod = self._dot(self._back[-1][1], val) if self._back else val
+        self._back.append((val, prod))
     
     def appendleft(self, val):
-        if self._front:
-            self._front.append((val, self._dot(val, self._front[-1][1])))
-        else:
-            self._front.append((val, val))
+        prod = self._dot(val, self._front[-1][1]) if self._front else val
+        self._front.append((val, prod))
+
+    def _distribute(self):
+        N = len(self._front)
+        N2 = N - 1 >> 1
+        prod = self._front[N2][0]
+        self._back.append((prod, prod))
+        for val, _ in self._front[:N2][::-1]:
+            prod = dot(prod, val)
+            self._back.append((val, prod))
+        prod = self._front[N2 + 1][0]
+        front = [(prod, prod)]
+        for val, _ in self._front[N2 + 2:]:
+            prod = dot(val, prod)
+            front.append((val, prod))
+        self._front = front
 
     def pop(self):
         if not self._back:
-            N = len(self._front)
-            if N == 1:
+            if len(self._front) == 1:
                 return self._front.pop()[0]
-            N2 = N - 1 >> 1
-            front, back, dot = self._front, self._back, self._dot
-            prod = front[N2][0]
-            back.append((prod, prod))
-            for val, _ in front[:N2][::-1]:
-                prod = dot(prod, val)
-                back.append((val, prod))
-            prod = front[N2 + 1][0]
-            _front = [(prod, prod)]
-            for val, _ in front[N2 + 2:]:
-                prod = dot(val, prod)
-                _front.append((val, prod))
-            self._front = _front
+            self._distribute()
         return self._back.pop()[0]
 
     def popleft(self):
         if not self._front:
-            N = len(self._back)
-            if N == 1:
+            if len(self._back) == 1:
                 return self._back.pop()[0]
-            N2 = N - 1 >> 1
-            front, back, dot = self._front, self._back, self._dot
-            prod = back[N2][0]
-            front.append((prod, prod))
-            for val, _ in back[:N2][::-1]:
-                prod = dot(val, prod)
-                front.append((val, prod))
-            prod = back[N2 + 1][0]
-            _back = [(prod, prod)]
-            for val, _ in back[N2 + 2:]:
-                prod = dot(prod, val)
-                _back.append((val, prod))
-            self._back = _back
+            self._back.reverse()
+            self._front, self._back = self._back, self._front
+            self._distribute()
         return self._front.pop()[0]
 
     def prod(self):
